04758b583b4742ff87b82a17c522a7a57aad66da
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-08-18 23:10:20 -0400

    Add MagnitudeValue constructor from Double
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- modules/math/shared/src/main/scala/gsp/math/MagnitudeValue.scala
+++ modules/math/shared/src/main/scala/gsp/math/MagnitudeValue.scala
@@ -5,20 +5,32 @@ package gsp.math
 
 import cats.{ Order, Show }
 import cats.instances.int._
+import spire.math.Rational
+import scala.math.rint
 
 /**
- * Exact magnitude value represented as an int with the original value scaled up
- *
- * @param scaledValue This magnitude integral value, as the original multiplied by 100. value is dimensionless
- * @see The Wikipedia [[https://en.wikipedia.org/wiki/Apparent_magnitude]]
- */
-final case class MagnitudeValue(private[gsp] val scaledValue: Int) extends Product with Serializable {
+  * Exact magnitude value represented as an int with the original value scaled up
+  *
+  * @param scaledValue This magnitude integral value, as the original multiplied by 100. value is dimensionless
+  * @see The Wikipedia [[https://en.wikipedia.org/wiki/Apparent_magnitude]]
+  */
+final case class MagnitudeValue(private[gsp] val scaledValue: Int)
+    extends Product
+    with Serializable {
+  def toRational: Rational  = Rational(scaledValue.toLong, 100)
   def toDoubleValue: Double = scaledValue / 100.0
 }
 
 object MagnitudeValue {
 
   final lazy val ZeroMagnitude = MagnitudeValue(0)
+
+  /**
+    * Construct a new MagnitudeValue of the given double value. Approximate.
+    * @group Constructors
+    */
+  def fromDouble(mg: Double): MagnitudeValue =
+    MagnitudeValue(rint(mg * 100).toInt)
 
   /** @group Typeclass Instances */
   implicit val MagnitudeValueShow: Show[MagnitudeValue] =
