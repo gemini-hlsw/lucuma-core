45499831a991a9a6edd47b0907f938718caf4d31
Raúl Piaggio <rpiaggio@gemini.edu>
2020-08-28 11:40:01 -0300

    Restore Place.

--- modules/core/shared/src/main/scala/lucuma/core/math/Place.scala
+++ modules/core/shared/src/main/scala/lucuma/core/math/Place.scala
@@ -10,13 +10,14 @@ import java.time.ZoneId
 import coulomb.Quantity
 import coulomb.si.Meter
 import eu.timepit.refined.types.numeric.NonNegInt
-import monocle.Getter
+import monocle.Lens
+import monocle.macros.GenLens
 import io.chrisdavenport.cats.time._
 import coulomb.cats.implicits._
 import eu.timepit.refined.cats._
 
 /** A point on Earth, given latitude, longitude and altitude in m above sea level. */
-class Place(
+final case class Place(
   val latitude:  Lat,
   val longitude: Lon,
   val altitude:  Quantity[NonNegInt, Meter],
@@ -27,35 +28,27 @@ class Place(
 }
 
 object Place {
-  def apply(
-    latitude:  Lat,
-    longitude: Lon,
-    altitude:  Quantity[NonNegInt, Meter],
-    timezone:  ZoneId
-  ): Place = new Place(latitude, longitude, altitude, timezone)
 
   /** @group Typeclass Instances */
   implicit val PlaceEqual: Eq[Place] =
     Eq.by(x => (x.latitude, x.longitude, x.altitude, x.timezone))
 
   /** @group Typeclass Instances */
-  implicit val PlaceShow: Show[Place] = Show.show(x =>
-    s"Place(${x.latitude.show}, ${x.longitude.show}, ${x.altitude.show}, ${x.timezone.show})"
-  )
+  implicit val PlaceShow: Show[Place] = Show.fromToString
 
   /** @group Optics */
-  val latitude: Getter[Place, Lat] =
-    Getter(_.latitude)
+  val latitude: Lens[Place, Lat] =
+    GenLens[Place](_.latitude)
 
   /** @group Optics */
-  val longitude: Getter[Place, Lon] =
-    Getter(_.longitude)
+  val longitude: Lens[Place, Lon] =
+    GenLens[Place](_.longitude)
 
   /** @group Optics */
-  val altitude: Getter[Place, Quantity[NonNegInt, Meter]] =
-    Getter(_.altitude)
+  val altitude: Lens[Place, Quantity[NonNegInt, Meter]] =
+    GenLens[Place](_.altitude)
 
   /** @group Optics */
-  val timezone: Getter[Place, ZoneId] =
-    Getter(_.timezone)
+  val timezone: Lens[Place, ZoneId] =
+    GenLens[Place](_.timezone)
 }
--- modules/tests/shared/src/test/scala/lucuma/core/math/PlaceSuite.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/math/PlaceSuite.scala
@@ -3,12 +3,36 @@
 
 package lucuma.core.math
 
+import cats.implicits._
+import cats.Show
+import coulomb.scalacheck.ArbQuantity._
+import coulomb.cats.implicits._
+import eu.timepit.refined.auto._
+import eu.timepit.refined.scalacheck.numeric._
+import eu.timepit.refined.cats._
 import lucuma.core.math.arb._
+import monocle.law.discipline._
+import io.chrisdavenport.cats.time._
+import org.scalacheck.Prop._
 import munit.DisciplineSuite
 import cats.kernel.laws.discipline.EqTests
 
 final class PlaceSuite extends DisciplineSuite {
   import ArbPlace._
+  import ArbDeclination._
+  import ArbAngle._
+  import ArbTime._
 
   checkAll("Place", EqTests[Place].eqv)
+
+  test("Show must be natural") {
+    forAll { a: Place =>
+      assertEquals(a.toString, Show[Place].show(a))
+    }
+  }
+
+  checkAll("Place.latitude", LensTests(Place.latitude))
+  checkAll("Place.longitude", LensTests(Place.longitude))
+  checkAll("Place.altitude", LensTests(Place.altitude))
+  checkAll("Place.timezone", LensTests(Place.timezone))
 }
