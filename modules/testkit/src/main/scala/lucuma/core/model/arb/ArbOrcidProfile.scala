c7e79b9282609cb77e682a6ec830e7c4e30a4177
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-10-14 11:38:56 -0300

    Add Eq instance for OrcidProfile and tests
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

---
+++ modules/testkit/src/main/scala/lucuma/core/model/arb/ArbOrcidProfile.scala
@@ -0,0 +1,32 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model.arb
+
+import lucuma.core.model.OrcidId
+import lucuma.core.model.OrcidProfile
+import org.scalacheck._
+import org.scalacheck.Arbitrary._
+
+trait ArbOrcidProfile {
+  import ArbOrcidId._
+
+  implicit val ArbOrcidProfile: Arbitrary[OrcidProfile] =
+    Arbitrary {
+      for {
+        id           <- arbitrary[OrcidId]
+        givenName    <- arbitrary[Option[String]]
+        familyName   <- arbitrary[Option[String]]
+        creditName   <- arbitrary[Option[String]]
+        primaryEmail <- arbitrary[Option[String]]
+      } yield OrcidProfile(id, givenName, familyName, creditName, primaryEmail)
+    }
+
+  implicit val CogOrcidProfile: Cogen[OrcidProfile] =
+    Cogen[(OrcidId, Option[String], Option[String], Option[String], Option[String])].contramap(x =>
+      (x.orcidId, x.givenName, x.familyName, x.creditName, x.primaryEmail)
+    )
+
+}
+
+object ArbOrcidProfile extends ArbOrcidProfile
---
+++ modules/tests/shared/src/test/scala/lucuma/core/model/OrcidIdProfile.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model
+
+import lucuma.core.model.arb._
+
+import munit._
+import cats.kernel.laws.discipline.EqTests
+
+final class OrcidProfileSuite extends DisciplineSuite {
+  import ArbOrcidProfile._
+
+  // Laws
+  checkAll("OrcidProfile", EqTests[OrcidProfile].eqv)
+
+}
