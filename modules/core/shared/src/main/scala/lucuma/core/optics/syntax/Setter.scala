4e8537d975b3629bfc0f321cd4b120e0ad574a96
Shane Walker <swalker@gemini.edu>
2021-01-15 10:34:17 -0300

    SetterOps

---
+++ modules/core/shared/src/main/scala/lucuma/core/optics/syntax/Setter.scala
@@ -0,0 +1,31 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.optics.syntax
+
+import cats.data.State
+import monocle.Setter
+import monocle.state.all._
+
+final class SetterOps[S, A](val self: Setter[S, A]) extends AnyVal {
+
+  def edit(a: A): State[S, Unit] =
+    self.assign_(a)
+
+  @inline def :=(a: A): State[S, Unit] =
+    edit(a)
+
+  def edit(a: Option[A]): State[S, Unit] =
+    a.fold(State.pure[S, Unit](()))(self.assign_(_))
+
+  @inline def :=(a: Option[A]): State[S, Unit] =
+    edit(a)
+
+}
+
+trait ToSetterOps {
+  implicit def ToSetterOps[S, A](s: Setter[S, A]): SetterOps[S, A] =
+    new SetterOps(s)
+}
+
+object setter extends ToSetterOps
