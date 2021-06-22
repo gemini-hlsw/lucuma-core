331f3fa6d8b153de9686ef6e5dbccf6c401e4945
Shane Walker <swalker@gemini.edu>
2021-06-22 12:38:20 -0400

    Add inactive obs statuses

--- modules/core/shared/src/main/scala/lucuma/core/enum/ObsStatus.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enum/ObsStatus.scala
@@ -11,18 +11,32 @@ import lucuma.core.util.Display
 sealed abstract class ObsStatus(val label: String) extends Product with Serializable
 
 object ObsStatus {
-  case object New       extends ObsStatus("New")
-  case object Included  extends ObsStatus("Included")
-  case object Proposed  extends ObsStatus("Proposed")
-  case object Approved  extends ObsStatus("Approved")
-  case object ForReview extends ObsStatus("For Review")
-  case object Ready     extends ObsStatus("Ready")
-  case object Ongoing   extends ObsStatus("Ongoing")
-  case object Observed  extends ObsStatus("Observed")
+  case object New             extends ObsStatus("New")
+  case object Included        extends ObsStatus("Included")
+  case object Proposed        extends ObsStatus("Proposed")
+  case object Approved        extends ObsStatus("Approved")
+  case object ForReview       extends ObsStatus("For Review")
+  case object Ready           extends ObsStatus("Ready")
+  case object Ongoing         extends ObsStatus("Ongoing")
+  case object InactiveReady   extends ObsStatus("Inactive (Ready)")
+  case object InactiveOngoing extends ObsStatus("Inactive (Ongoing)")
+  case object Observed        extends ObsStatus("Observed")
 
   /** @group Typeclass Instances */
   implicit val ObsStatusEnumerated: Enumerated[ObsStatus] =
-    Enumerated.of(New, Included, Proposed, Approved, ForReview, Ready, Ongoing, Observed)
+    Enumerated.of(
+      New,
+      Included,
+      Proposed,
+      Approved,
+      ForReview,
+      Ready,
+      Ongoing,
+      InactiveReady,
+      InactiveOngoing,
+      Observed
+    )
 
-  implicit val ObsStatusDisplay: Display[ObsStatus] = Display.byShortName(_.label)
+  implicit val ObsStatusDisplay: Display[ObsStatus] =
+    Display.byShortName(_.label)
 }
