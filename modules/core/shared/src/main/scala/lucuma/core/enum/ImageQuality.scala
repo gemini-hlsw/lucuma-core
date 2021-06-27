4b2b3fe3912f87d2c9e82970fc297e152e19fccf
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2021-06-27 11:23:33 -0400

    Use a coulomb quantity for image quality
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- modules/core/shared/src/main/scala/lucuma/core/enum/ImageQuality.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enum/ImageQuality.scala
@@ -3,23 +3,27 @@
 
 package lucuma.core.enum
 
+import coulomb._
+import coulomb.accepted.ArcSecond
+import lucuma.core.math.units.DeciArcSecond
 import lucuma.core.util.{ Display, Enumerated }
+import spire.math.Rational
 
-sealed abstract class ImageQuality(val toDeciArcSeconds: Int) extends Product with Serializable {
-  def toArcSeconds: Double = toDeciArcSeconds / 10.0
-  def label: String        = f"""< $toArcSeconds%.1f\""""
+sealed abstract class ImageQuality(val toDeciArcSeconds: Quantity[Rational, DeciArcSecond]) extends Product with Serializable {
+  def toArcSeconds: Quantity[Rational, ArcSecond] = toDeciArcSeconds.toUnit[ArcSecond]
+  def label: String        = f"""< ${toArcSeconds.value.toDouble}%.1f\""""
 }
 
 object ImageQuality {
-  case object PointOne     extends ImageQuality(1)
-  case object PointTwo     extends ImageQuality(2)
-  case object PointThree   extends ImageQuality(3)
-  case object PointFour    extends ImageQuality(4)
-  case object PointSix     extends ImageQuality(6)
-  case object PointEight   extends ImageQuality(8)
-  case object OnePointZero extends ImageQuality(10)
-  case object OnePointFive extends ImageQuality(15)
-  case object TwoPointZero extends ImageQuality(20)
+  case object PointOne     extends ImageQuality(1.withUnit[DeciArcSecond])
+  case object PointTwo     extends ImageQuality(2.withUnit[DeciArcSecond])
+  case object PointThree   extends ImageQuality(3.withUnit[DeciArcSecond])
+  case object PointFour    extends ImageQuality(4.withUnit[DeciArcSecond])
+  case object PointSix     extends ImageQuality(6.withUnit[DeciArcSecond])
+  case object PointEight   extends ImageQuality(8.withUnit[DeciArcSecond])
+  case object OnePointZero extends ImageQuality(10.withUnit[DeciArcSecond])
+  case object OnePointFive extends ImageQuality(15.withUnit[DeciArcSecond])
+  case object TwoPointZero extends ImageQuality(20.withUnit[DeciArcSecond])
 
   implicit val ImageQualityEnumerated: Enumerated[ImageQuality] =
     Enumerated.of(PointOne,
