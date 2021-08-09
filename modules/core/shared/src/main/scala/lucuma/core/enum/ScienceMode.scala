ccc775450c919745c55236e802f6c048cf0296f7
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2021-08-09 16:53:24 -0400

    Move some enums from lucuma-tmp-api
    Signed-off-by: Carlos Quiroz <carlos.quiroz@noirlab.edu>

---
+++ modules/core/shared/src/main/scala/lucuma/core/enum/ScienceMode.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.enum
+
+import lucuma.core.util.Enumerated
+
+sealed abstract class ScienceMode extends Product with Serializable
+
+object ScienceMode {
+  case object Imaging      extends ScienceMode
+  case object Spectroscopy extends ScienceMode
+
+  implicit val ScienceModeEnumerated: Enumerated[ScienceMode] =
+    Enumerated.of(Imaging, Spectroscopy)
+}
+
---
+++ modules/core/shared/src/main/scala/lucuma/core/enum/SpectroscopyCapabilities.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.enum
+
+import lucuma.core.util.Enumerated
+
+sealed abstract class SpectroscopyCapabilities extends Product with Serializable
+
+object SpectroscopyCapabilities {
+  case object NodAndShuffle extends SpectroscopyCapabilities
+  case object Polarimetry   extends SpectroscopyCapabilities
+  case object Corongraphy   extends SpectroscopyCapabilities
+
+  implicit val SpectroscopyCapabilitiesEnumerated: Enumerated[SpectroscopyCapabilities] =
+    Enumerated.of(NodAndShuffle, Polarimetry, Corongraphy)
+}
