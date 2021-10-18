be118150edb0a05fe3415a47e7f8ef849c2c513c
Javier Lührs <jluhrs@gemini.edu>
2021-10-18 17:03:24 -0300

    Added some enums to be used with the ObservationDB schema.

---
+++ modules/core/shared/src/main/scala/lucuma/core/enum/SequenceCommand.scala
@@ -0,0 +1,27 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.`enum`
+
+import lucuma.core.util.Enumerated
+
+sealed trait SequenceCommand extends Product with Serializable
+
+object SequenceCommand {
+  /** @group Constructors */ case object Abort extends SequenceCommand
+  /** @group Constructors */ case object Continue extends SequenceCommand
+  /** @group Constructors */ case object Pause extends SequenceCommand
+  /** @group Constructors */ case object Slew extends SequenceCommand
+  /** @group Constructors */ case object Start extends SequenceCommand
+  /** @group Constructors */ case object Stop extends SequenceCommand
+
+  implicit val SequenceCommandEnumerated: Enumerated[SequenceCommand] =
+    Enumerated.of(
+      Abort,
+      Continue,
+      Pause,
+      Slew,
+      Start,
+      Stop
+    )
+}
