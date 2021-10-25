3993fefe9ea30dd98b78df63050481eaa00ecfcd
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2021-10-25 11:45:05 -0300

    Internalize AccessControlException
    Signed-off-by: Carlos Quiroz <carlos.quiroz@noirlab.edu>

---
+++ modules/core/shared/src/main/scala/lucuma/core/model/AccessControlException.scala
@@ -0,0 +1,7 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model
+
+final case class AccessControlException(displayName: String, id: User.Id, role: Role, access: Access) extends
+  Exception(s"$displayName (User ${id.value}, $role) does not have required access $access.")
