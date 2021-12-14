f8966315bd3369848d636c3074aa5f9cc1553a2f
Raúl Piaggio <rpiaggio@gemini.edu>
2021-12-14 17:47:36 -0300

    Remove commented code.

--- modules/testkit/src/main/scala/lucuma/core/math/dimensional/arb/ArbQty.scala
+++ modules/testkit/src/main/scala/lucuma/core/math/dimensional/arb/ArbQty.scala
@@ -3,7 +3,6 @@
 
 package lucuma.core.math.dimensional.arb
 
-// import coulomb.define.UnitDefinition
 import coulomb.define.UnitDefinition
 import lucuma.core.math.dimensional._
 import org.scalacheck.Arbitrary._
