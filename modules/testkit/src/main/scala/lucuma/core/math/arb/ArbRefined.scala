ebbfa8cc2f162ab61cd6f32aa30a1c3951f2de4b
Raúl Piaggio <rpiaggio@gemini.edu>
2021-12-22 23:19:38 -0300

    Avoid arbPosBigDecimal fatigue.

--- modules/testkit/src/main/scala/lucuma/core/math/arb/ArbRefined.scala
+++ modules/testkit/src/main/scala/lucuma/core/math/arb/ArbRefined.scala
@@ -3,20 +3,24 @@
 
 package lucuma.core.math.arb
 
-import cats.syntax.all._
 import eu.timepit.refined.types.numeric.PosBigDecimal
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Cogen
 
 trait ArbRefined {
-  private val BigDecimalZero: BigDecimal = BigDecimal(0)
+  private val BigDecimalZero: BigDecimal      = BigDecimal(0)
+  private val PosBigDecimalOne: PosBigDecimal = PosBigDecimal(BigDecimal(1))
 
   // Refined does not derive this arbitrary. Generally deriving `Arbitrary` instances for `Pos`
   // numbers requires instances of `Adjacent` and `Max`, which don't exist for `BigDecimal`.
   implicit val arbPosBigDecimal: Arbitrary[PosBigDecimal] =
     Arbitrary(
-      arbitrary[BigDecimal].suchThat(_ =!= BigDecimalZero).map(x => PosBigDecimal.unsafeFrom(x.abs))
+      arbitrary[BigDecimal]
+        .map {
+          case BigDecimalZero => PosBigDecimalOne
+          case x              => PosBigDecimal.unsafeFrom(x.abs)
+        }
     )
 
   implicit val cogenPosBigDecimal: Cogen[PosBigDecimal] =
