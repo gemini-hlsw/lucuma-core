448dde4b55a561c42d513d9c6816bcb869176d86
Shane Walker <swalker@gemini.edu>
2022-02-10 11:17:52 -0300

    add ImageQuality.toAngle

--- modules/core/shared/src/main/scala/lucuma/core/enum/ImageQuality.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enum/ImageQuality.scala
@@ -1,14 +1,16 @@
 // Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package lucuma.core.enum
+package lucuma.core.`enum`
 
 import coulomb._
 import coulomb.accepted.ArcSecond
+import coulomb.refined._
 import eu.timepit.refined._
 import eu.timepit.refined.numeric.Positive
 import eu.timepit.refined.types.numeric.PosInt
-import lucuma.core.math.units.DeciArcSecond
+import lucuma.core.math.Angle
+import lucuma.core.math.units.{DeciArcSecond, MicroArcSecond}
 import lucuma.core.util.Display
 import lucuma.core.util.Enumerated
 import spire.math.Rational
@@ -16,6 +18,9 @@ import spire.math.Rational
 sealed abstract class ImageQuality(val toDeciArcSeconds: Quantity[PosInt, DeciArcSecond]) extends Product with Serializable {
   def toArcSeconds: Quantity[Rational, ArcSecond] = toDeciArcSeconds.to[Rational, ArcSecond]
   def label: String        = f"""< ${toArcSeconds.value.toDouble}%.1f\""""
+
+  def toAngle: Angle =
+    Angle.fromMicroarcseconds(toDeciArcSeconds.to[PosInt, MicroArcSecond].value.value)
 }
 
 object ImageQuality {
