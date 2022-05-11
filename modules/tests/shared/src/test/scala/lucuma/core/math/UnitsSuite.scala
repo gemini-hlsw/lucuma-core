0399df4049fbb9de9fea969ad1bf18a98f3f4534
Arman Bilge <armanbilge@gmail.com>
2022-05-11 19:20:57 +0000

    More tests compiling, but do they pass?

--- modules/tests/shared/src/test/scala/lucuma/core/math/UnitsSuite.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/math/UnitsSuite.scala
@@ -17,33 +17,33 @@ final class UnitsSuite extends FunSuite {
 
   test("Sanity tests") {
     assert(Rational.one.withUnit[Angstrom] === Rational(1, SafeLong(10).pow(10)).withUnit[Meter])
-    // assert(
-    //   Rational.one.withUnit[Jansky] === Rational(1, SafeLong(10).pow(26))
-    //     .withUnit[Watt %/ (Meter %^ 2) %* Hertz]
-    // )
-    // assert(Rational.one.withUnit[Erg] === Rational(1, SafeLong(10).pow(7)).withUnit[Joule])
+    assert(
+      Rational.one.withUnit[Jansky] === Rational(1, SafeLong(10).pow(26))
+        .withUnit[Watt / (Meter ^ 2) * Hertz]
+    )
+    assert(Rational.one.withUnit[Erg] === Rational(1, SafeLong(10).pow(7)).withUnit[Joule])
   }
 
-  // test("Check brightness unit names") {
-  //   assertEquals(1.withUnit[VegaMagnitude].show, "1 Vega mag")
-  //   assertEquals(1.withUnit[ABMagnitude].show, "1 AB mag")
-  //   assertEquals(1.withUnit[Jansky].show, "1 Jy")
-  //   assertEquals(1.withUnit[WattsPerMeter2Micrometer].show, "1 W/m²/µm")
-  //   assertEquals(1.withUnit[ErgsPerSecondCentimeter2Angstrom].show, "1 erg/s/cm²/Å")
-  //   assertEquals(1.withUnit[ErgsPerSecondCentimeter2Hertz].show, "1 erg/s/cm²/Hz")
-  //   assertEquals(1.withUnit[VegaMagnitudePerArcsec2].show, "1 Vega mag/arcsec²")
-  //   assertEquals(1.withUnit[ABMagnitudePerArcsec2].show, "1 AB mag/arcsec²")
-  //   assertEquals(1.withUnit[JanskyPerArcsec2].show, "1 Jy/arcsec²")
-  //   assertEquals(1.withUnit[WattsPerMeter2MicrometerArcsec2].show, "1 W/m²/µm/arcsec²")
-  //   assertEquals(
-  //     1.withUnit[ErgsPerSecondCentimeter2AngstromArcsec2].show,
-  //     "1 erg/s/cm²/Å/arcsec²"
-  //   )
-  //   assertEquals(1.withUnit[ErgsPerSecondCentimeter2HertzArcsec2].show, "1 erg/s/cm²/Hz/arcsec²")
-  //   assertEquals(1.withUnit[WattsPerMeter2].show, "1 W/m²")
-  //   assertEquals(1.withUnit[ErgsPerSecondCentimeter2].show, "1 erg/s/cm²")
-  //   assertEquals(1.withUnit[WattsPerMeter2Arcsec2].show, "1 W/m²/arcsec²")
-  //   assertEquals(1.withUnit[ErgsPerSecondCentimeter2Arcsec2].show, "1 erg/s/cm²/arcsec²")
-  // }
+  test("Check brightness unit names") {
+    assertEquals(1.withUnit[VegaMagnitude].show, "1 Vega mag")
+    assertEquals(1.withUnit[ABMagnitude].show, "1 AB mag")
+    assertEquals(1.withUnit[Jansky].show, "1 Jy")
+    assertEquals(1.withUnit[WattsPerMeter2Micrometer].show, "1 W/m²/µm")
+    assertEquals(1.withUnit[ErgsPerSecondCentimeter2Angstrom].show, "1 erg/s/cm²/Å")
+    assertEquals(1.withUnit[ErgsPerSecondCentimeter2Hertz].show, "1 erg/s/cm²/Hz")
+    assertEquals(1.withUnit[VegaMagnitudePerArcsec2].show, "1 Vega mag/arcsec²")
+    assertEquals(1.withUnit[ABMagnitudePerArcsec2].show, "1 AB mag/arcsec²")
+    assertEquals(1.withUnit[JanskyPerArcsec2].show, "1 Jy/arcsec²")
+    assertEquals(1.withUnit[WattsPerMeter2MicrometerArcsec2].show, "1 W/m²/µm/arcsec²")
+    assertEquals(
+      1.withUnit[ErgsPerSecondCentimeter2AngstromArcsec2].show,
+      "1 erg/s/cm²/Å/arcsec²"
+    )
+    assertEquals(1.withUnit[ErgsPerSecondCentimeter2HertzArcsec2].show, "1 erg/s/cm²/Hz/arcsec²")
+    assertEquals(1.withUnit[WattsPerMeter2].show, "1 W/m²")
+    assertEquals(1.withUnit[ErgsPerSecondCentimeter2].show, "1 erg/s/cm²")
+    assertEquals(1.withUnit[WattsPerMeter2Arcsec2].show, "1 W/m²/arcsec²")
+    assertEquals(1.withUnit[ErgsPerSecondCentimeter2Arcsec2].show, "1 erg/s/cm²/arcsec²")
+  }
 
 }
