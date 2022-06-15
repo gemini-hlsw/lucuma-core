09f9fef3a6e7ad49f743778ef5068fa98cefab47
Raúl Piaggio <rpiaggio@gemini.edu>
2022-06-14 18:35:48 -0700

    Add test

--- modules/tests/shared/src/test/scala/lucuma/core/math/truncated/TruncatedAngleSpec.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/math/truncated/TruncatedAngleSpec.scala
@@ -4,9 +4,18 @@
 package lucuma.core.math.truncated
 
 import cats.kernel.laws.discipline.EqTests
+import lucuma.core.math.arb.ArbAngle
 import lucuma.core.math.truncated.arb.ArbTruncatedAngle._
+import lucuma.core.optics.laws.discipline.SplitEpiTests
 import munit.DisciplineSuite
+import org.scalacheck.Arbitrary._
 
 class TruncatedAngleSpec extends DisciplineSuite {
+  import ArbAngle._
+
+  // Laws
   checkAll("Eq[TruncatedAngle]", EqTests[TruncatedAngle].eqv)
+
+  // Optics
+  checkAll("TruncatedAngle.angle", SplitEpiTests(TruncatedAngle.angle).splitEpi)
 }
