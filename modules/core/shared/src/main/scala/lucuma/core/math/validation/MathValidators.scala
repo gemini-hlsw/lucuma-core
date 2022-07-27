e868f9002144b9c17ff06503d08b39ded458c831
Todd Burnside <tburnside@gemini.edu>
2022-07-27 13:07:03 -0400

    Fix indentation

--- modules/core/shared/src/main/scala/lucuma/core/math/validation/MathValidators.scala
+++ modules/core/shared/src/main/scala/lucuma/core/math/validation/MathValidators.scala
@@ -17,8 +17,8 @@ trait MathValidators {
   val epoch: InputValidSplitEpi[Epoch] =
     InputValidSplitEpi.fromPrism(Epoch.fromString, "Invalid epoch")
 
-    val epochNoScheme:InputValidWedge[Epoch] = 
-      InputValidWedge.fromFormat(Epoch.fromStringNoScheme, "Invalid epoch")
+  val epochNoScheme:InputValidWedge[Epoch] = 
+    InputValidWedge.fromFormat(Epoch.fromStringNoScheme, "Invalid epoch")
 
   val angleArcSec: InputValidSplitEpi[Angle] =
     InputValidSplitEpi(
