e57c9aba115ced39f75790c815bff78f1a1d9bfe
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-10-18 20:02:30 -0300

    Add csv as a possible catalog name

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / tlBaseVersion                         := "0.57"
+ThisBuild / tlBaseVersion                         := "0.58"
 ThisBuild / tlCiReleaseBranches                   := Seq("master")
 ThisBuild / githubWorkflowEnv += "MUNIT_FLAKY_OK" -> "true"
 
--- modules/core/shared/src/main/scala/lucuma/core/enums/CatalogName.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/CatalogName.scala
@@ -5,14 +5,7 @@ package lucuma.core.enums
 
 import lucuma.core.util.Enumerated
 
-sealed abstract class CatalogName(val tag: String) extends Product with Serializable
-
-object CatalogName {
-  case object Simbad extends CatalogName("simbad")
-  case object Gaia extends CatalogName("gaia")
-
-  /** @group Typeclass Instances */
-  implicit val CatalogNameEnumerated: Enumerated[CatalogName] =
-    Enumerated.from(Simbad, Gaia).withTag(_.tag)
-
-}
+enum CatalogName(val tag: String) derives Enumerated:
+  case Simbad extends CatalogName("simbad")
+  case Gaia extends CatalogName("gaia")
+  case Import extends CatalogName("import")
