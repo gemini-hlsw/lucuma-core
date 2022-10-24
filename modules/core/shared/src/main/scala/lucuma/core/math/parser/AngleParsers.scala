5114695077a08757de070b41e49df4f179f1b0dc
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-10-24 19:19:20 -0300

    Better tests for the parsers

--- modules/core/shared/src/main/scala/lucuma/core/math/parser/AngleParsers.scala
+++ modules/core/shared/src/main/scala/lucuma/core/math/parser/AngleParsers.scala
@@ -15,11 +15,11 @@ import lucuma.core.math.RightAscension
 import lucuma.core.optics.Format
 
 trait AngleParsers:
-  val colon        = char(':')
-  val colonOrSpace = (char(':') | sp).void
-  val neg = char('-').map(_ => true).backtrack | char('+').?.map(_ => false)
+  val colon: Parser[Unit]        = char(':')
+  val colonOrSpace: Parser[Unit] = (char(':') | sp).void
+  val neg: Parser0[Boolean]      = char('-').map(_ => true).backtrack | char('+').?.map(_ => false)
 
-  val hours = (char('1') ~ digit).backtrack // 10-19
+  val hours: Parser0[Int] = (char('1') ~ digit).backtrack // 10-19
     .orElse(char('2') ~ charIn('0' to '4')) // 20-24
     .backtrack
     .orElse(char('0').? *> digit)           // 00 - 09
@@ -29,7 +29,7 @@ trait AngleParsers:
     .map(_.toInt)
     .withContext("hours")
 
-  val minutes = (charIn('0' to '5') ~ digit).backtrack // 10-19
+  val minutes: Parser0[Int] = (charIn('0' to '5') ~ digit).backtrack // 10-19
     .orElse(char('5') ~ digit)          // 20-24
     .backtrack
     .orElse(char('0').?.with1 *> digit) // 00 - 09
@@ -39,16 +39,13 @@ trait AngleParsers:
     .map(_.toInt)
     .withContext("minutes")
 
-  val seconds =
+  private[parser] val seconds =
     // allow any amount of decimals but only use 6
     (minutes ~ char('.').? ~ digit.rep(1, 3).?.string ~ digit.rep(1, 3).?.string ~ digit.rep.?)
       .map { case ((((s, _), d1), d2), _) =>
-        (s,
-         if (d1.isEmpty) BigDecimal(0) else BigDecimal(d1.padTo(3, '0')),
-         if (d2.isEmpty) BigDecimal(0) else BigDecimal(d2)
-        )
+        (s, if (d1.isEmpty) 0 else d1.padTo(3, '0').toInt, if (d2.isEmpty) 0 else d2.toInt)
       }
-    .withContext("seconds")
+      .withContext("seconds")
 
   private val hmsParser1 =
     (hours ~ colonOrSpace.void ~ minutes ~ colonOrSpace.void ~ seconds).map {
@@ -61,29 +58,29 @@ trait AngleParsers:
       minutes ~ (char('m') ~ sp.?).void ~
       seconds ~ char('s'))
       .map { case (((((h, _), m), _), (s, ms, µs)), _) =>
-        HourAngle.fromHMS(h, m, s, ms.toInt, µs.toInt)
+        HourAngle.fromHMS(h, m, s, ms, µs)
       }
 
-  val hms = (hmsParser1.backtrack | hmsParser2)
+  val hms: Parser0[HourAngle] = (hmsParser1.backtrack | hmsParser2)
     .withContext("hms")
 
   val degrees =
-    (char('3') ~ charIn('0' to '5') ~ digit) // 300-359
-    .backtrack
-    .orElse(charIn('1' to '2') ~ digit ~ digit) // 100-299
-    .backtrack
-    .orElse(charIn('0' to '9') ~ digit) // 10-99
-    .backtrack
-    .orElse(char('0').? *> digit) // 00 - 09
-    .backtrack
-    .orElse(char('0'))            // plain 0
-    .string
-    .map(_.toInt)
+    (char('3') ~ charIn('0' to '5') ~ digit)      // 300-359
+      .backtrack
+      .orElse(charIn('1' to '2') ~ digit ~ digit) // 100-299
+      .backtrack
+      .orElse(charIn('0' to '9') ~ digit)         // 10-99
+      .backtrack
+      .orElse(char('0').? *> digit)               // 00 - 09
+      .backtrack
+      .orElse(char('0'))                          // plain 0
+      .string
+      .map(_.toInt)
 
   private val dmsParser1 =
     (neg ~ degrees ~ colonOrSpace ~ minutes ~ colonOrSpace ~ seconds).map {
-      case (((((neg, h), _), m), _), (s, ms, µs)) =>
-        val r = Angle.fromDMS(h, m, s, ms.toInt, µs.toInt)
+      case (((((neg, d), _), m), _), (s, ms, µs)) =>
+        val r = Angle.fromDMS(d, m, s, ms, µs)
         if (neg) -r else r
     }
 
@@ -91,11 +88,11 @@ trait AngleParsers:
     (neg ~ degrees ~ (char('°') ~ sp.?).void ~
       minutes ~ (char('′') ~ sp.?).void ~
       seconds ~ char('″').void).map { case ((((((neg, h), _), m), _), (s, ms, µs)), _) =>
-      val r = Angle.fromDMS(h, m, s, ms.toInt, µs.toInt)
+      val r = Angle.fromDMS(h, m, s, ms, µs)
       if (neg) -r else r
     }
 
-  val dms = (dmsParser1.backtrack | dmsParser2)
+  val dms: Parser0[Angle] = (dmsParser1.backtrack | dmsParser2)
     .withContext("dms")
 
 object AngleParsers extends AngleParsers
--- modules/tests/shared/src/test/scala/lucuma/core/math/parser/AngleParsersSuite.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/math/parser/AngleParsersSuite.scala
@@ -4,57 +4,58 @@
 package lucuma.core.math.parser
 
 import cats.kernel.laws.discipline.*
+import cats.syntax.all.*
+import lucuma.core.math.Angle
 import lucuma.core.math.Declination
+import lucuma.core.math.HourAngle
 import lucuma.core.math.arb.ArbAngle
 import lucuma.core.math.arb.ArbRightAscension.*
 import lucuma.core.optics.laws.discipline.FormatTests
 import munit.*
 
 class AngleParsersSuite extends DisciplineSuite with AngleParsers:
-  assert(hours.parseAll("0").isRight)
-  assert(hours.parseAll("09").isRight)
-  assert(hours.parseAll("1").isRight)
-  assert(hours.parseAll("01").isRight)
-  assert(hours.parseAll("10").isRight)
-  assert(hours.parseAll("19").isRight)
-  assert(hours.parseAll("22").isRight)
+  assertEquals(hours.parseAll("0"), 0.asRight)
+  assertEquals(hours.parseAll("09"), 9.asRight)
+  assertEquals(hours.parseAll("1"), 1.asRight)
+  assertEquals(hours.parseAll("01"), 1.asRight)
+  assertEquals(hours.parseAll("10"), 10.asRight)
+  assertEquals(hours.parseAll("19"), 19.asRight)
+  assertEquals(hours.parseAll("22"), 22.asRight)
   assert(hours.parseAll("25").isLeft)
 
-  assert(minutes.parseAll("0").isRight)
-  assert(minutes.parseAll("09").isRight)
-  assert(minutes.parseAll("1").isRight)
-  assert(minutes.parseAll("01").isRight)
-  assert(minutes.parseAll("10").isRight)
-  assert(minutes.parseAll("19").isRight)
-  assert(minutes.parseAll("22").isRight)
-  assert(minutes.parseAll("59").isRight)
+  assertEquals(minutes.parseAll("0"), 0.asRight)
+  assertEquals(minutes.parseAll("09"), 9.asRight)
+  assertEquals(minutes.parseAll("1"), 1.asRight)
+  assertEquals(minutes.parseAll("01"), 1.asRight)
+  assertEquals(minutes.parseAll("10"), 10.asRight)
+  assertEquals(minutes.parseAll("19"), 19.asRight)
+  assertEquals(minutes.parseAll("22"), 22.asRight)
+  assertEquals(minutes.parseAll("59"), 59.asRight)
   assert(minutes.parseAll("60").isLeft)
 
-  assert(seconds.parseAll("59").isRight)
-  assert(seconds.parseAll("59.").isRight)
-  assert(seconds.parseAll("59.1").isRight)
-  assert(seconds.parseAll("59.00000000000000").isRight)
-  assert(seconds.parseAll("59.1234567890").isRight)
+  assertEquals(seconds.parseAll("59"), (59, 0, 0).asRight)
+  assertEquals(seconds.parseAll("59."), (59, 0, 0).asRight)
+  assertEquals(seconds.parseAll("59.1"), (59, 100, 0).asRight)
+  assertEquals(seconds.parseAll("59.00000000000000"), (59, 0, 0).asRight)
+  assertEquals(seconds.parseAll("59.1234567890"), (59, 123, 456).asRight)
 
-  assert(hms.parseAll("00:00:00.000000").isRight)
-  assert(hms.parseAll("01:51:51.10").isRight)
-  assert(hms.parseAll("01:51:51.100").isRight)
-  assert(hms.parseAll("01:51:51.1000").isRight)
-  assert(hms.parseAll("01:51:51.1000100").isRight)
-  assert(hms.parseAll("14:29:42.9461331854").isRight)
-  assert(hms.parseAll("14:29:42").isRight)
-
-  assert(hms.parseAll("14h 29m 42s").isRight)
-
+  assertEquals(hms.parseAll("00:00:00.000000"), HourAngle.angle.reverseGet(Angle.Angle0).asRight)
+  assertEquals(hms.parseAll("01:51:51.10"), HourAngle.fromHMS(1, 51, 51, 100, 0).asRight)
+  assertEquals(hms.parseAll("01:51:51.1000"), HourAngle.fromHMS(1, 51, 51, 100, 0).asRight)
+  assertEquals(hms.parseAll("01:51:51.1000100"), HourAngle.fromHMS(1, 51, 51, 100, 10).asRight)
+  assertEquals(hms.parseAll("14:29:42.9461331854"), HourAngle.fromHMS(14, 29, 42, 946, 133).asRight)
+  assertEquals(hms.parseAll("14 29 42.9461331854"), HourAngle.fromHMS(14, 29, 42, 946, 133).asRight)
+  assertEquals(hms.parseAll("14h 29m 42.9461331854s"), HourAngle.fromHMS(14, 29, 42, 946, 133).asRight)
   assert(neg.parseAll("-").isRight)
   assert(neg.parseAll("+").isRight)
   assert(neg.parseAll("").isRight)
-  assert(dms.parseAll("14:29:42").isRight)
-  assert(dms.parseAll("-16:27:46.522175847").isRight)
-  assert(dms.parseAll("-16 27 46.522175847").isRight)
-  assert(dms.parseAll("+16 27 46.522175847").isRight)
-  assert(dms.parseAll("-16° 27′ 46.522175847″").isRight)
-  assert(dms.parseAll("-148 00 54.775808").isRight)
-  assert(dms.parseAll("-248 00 54.775808").isRight)
-  assert(dms.parseAll("+248 00 54.775808").isRight)
-  assert(dms.parseAll("+320 00 54.775808").isRight)
+
+  assertEquals(dms.parseAll("14:29:42"), Angle.fromDMS(14, 29, 42, 0, 0).asRight)
+  assertEquals(dms.parseAll("-16:27:46.522175847"), (-Angle.fromDMS(16, 27, 46, 522, 175)).asRight)
+  assertEquals(dms.parseAll("+16:27:46.522175847"), Angle.fromDMS(16, 27, 46, 522, 175).asRight)
+  assertEquals(dms.parseAll("16° 27′ 46.522175847″"), Angle.fromDMS(16, 27, 46, 522, 175).asRight)
+  assertEquals(dms.parseAll("-148 00 54.775808"), (-Angle.fromDMS(148, 0, 54, 775, 808)).asRight)
+  assertEquals(dms.parseAll("-248 00 54.775808"), (-Angle.fromDMS(248, 0, 54, 775, 808)).asRight)
+  assertEquals(dms.parseAll("+320 00 54.775808"), Angle.fromDMS(320, 0, 54, 775, 808).asRight)
+
+  assert(dms.parseAll("+360 00 54.775808").isLeft)
