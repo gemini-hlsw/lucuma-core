0b4daebfb4bd3d86d9dd47479610a7f6de71cffa
Raúl Piaggio <rpiaggio@gemini.edu>
2023-02-13 11:01:12 -0300

    RefinedNewType

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / tlBaseVersion                         := "0.66"
+ThisBuild / tlBaseVersion                         := "0.67"
 ThisBuild / tlCiReleaseBranches                   := Seq("master")
 ThisBuild / githubWorkflowEnv += "MUNIT_FLAKY_OK" -> "true"
 
--- modules/core/shared/src/main/scala/lucuma/core/math/package.scala
+++ modules/core/shared/src/main/scala/lucuma/core/math/package.scala
@@ -5,6 +5,9 @@ package lucuma
 package core
 package math
 
+import eu.timepit.refined.numeric.*
+import lucuma.core.util.RefinedNewType
+
 /** Mathematical data types for general use, not specific to the Gem model. */
 type RA = RightAscension
 val RA: RightAscension.type = RightAscension
@@ -17,3 +20,23 @@ val Lat: Declination.type = Declination
 
 type Lon = Angle
 val Lon: Angle.type = Angle
+
+// The sun is -27 magnitudes. Maximum in Janskys is 1e8.
+type BrightnessValueRefinement = Interval.Closed[-30, 100_000_000]
+object BrightnessValue extends RefinedNewType[BigDecimal, BrightnessValueRefinement]
+type BrightnessValue = BrightnessValue.Type
+
+// The line width must be positive. For upper limit, we could probably safely use 1e6 km/s.
+type LineWidthValueRefinement = Interval.OpenClosed[0, 1_000_000]
+object LineWidthValue extends RefinedNewType[BigDecimal, LineWidthValueRefinement]
+type LineWidthValue = LineWidthValue.Type
+
+// Should never be less than zero or larger than 1.
+type LineFluxValueRefinement = Interval.Closed[0, 1]
+object LineFluxValue extends RefinedNewType[BigDecimal, LineFluxValueRefinement]
+type LineFluxValue = LineFluxValue.Type
+
+// Should never be less than zero or larger than 1.
+type FluxDensityContinuumValueRefinement = Interval.Closed[0, 1]
+object FluxDensityContinuumValue extends RefinedNewType[BigDecimal, FluxDensityContinuumValueRefinement]
+type FluxDensityContinuumValue = FluxDensityContinuumValue.Type
