f16b9f838cdda27f8b41a40c04c01e381bccf271
Shane Walker <pistes.clary_0w@icloud.com>
2023-03-20 09:28:34 -0300

    Parsers

---
+++ modules/core/shared/src/main/scala/lucuma/core/util/parser/UtilParsers.scala
@@ -0,0 +1,20 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.util
+package parser
+
+import cats.parse.Parser0
+import lucuma.core.parser.MiscParsers.posBigDecimal
+
+trait UtilParsers {
+
+  /** Parses positive time spans in seconds. */
+  val posSecondsTimeSpan: Parser0[TimeSpan] =
+    posBigDecimal
+      .mapFilter(pdb => TimeSpan.fromSeconds(pdb.value))
+      .withContext("TimeSpan(seconds)")
+
+}
+
+object UtilParsers extends UtilParsers
