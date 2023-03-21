581b466ffa1400eef2f18d60ff732744e7419fd1
Shane Walker <pistes.clary_0w@icloud.com>
2023-03-21 17:50:45 -0300

    Update GMOS filters

---
+++ modules/core/shared/src/main/scala/lucuma/core/enums/ConvenienceOps.scala
@@ -0,0 +1,26 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.enums
+
+import lucuma.core.math.BoundedInterval
+import lucuma.core.math.Wavelength
+
+private[enums] trait ConvenienceOps {
+  extension (i: Int) {
+    def pm: Wavelength =
+      Wavelength.unsafeFromIntPicometers(i)
+
+    def pmRange: BoundedInterval[Wavelength] =
+      // This is open upper (instead of closed) because of a limitation in postgres int4range
+      BoundedInterval.unsafeOpenUpper(i.pm, Wavelength.Max)
+  }
+
+  extension (tup: (Int, Int)) {
+    def pmRange: BoundedInterval[Wavelength] =
+      BoundedInterval.unsafeOpenUpper(tup._1.pm, tup._2.pm)
+  }
+
+}
+
+private[enums] object ConvenienceOps extends ConvenienceOps
