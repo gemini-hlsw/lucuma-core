9a907c25fc2f73f0db54edcf9f90fb17da123f61
Shane Walker <swalker2m@users.noreply.github.com>
2023-05-16 12:07:25 -0400

    Planned time enums (#726)
    * planned-time related enums
    
    * comment
    
    ---------
    
    Co-authored-by: Shane Walker <pistes.clary_0w@icloud.com>

---
+++ modules/core/shared/src/main/scala/lucuma/core/enums/ChargeClass.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.enums
+
+import lucuma.core.util.Enumerated
+
+/**
+ * Indication of which entity (if any) is to be charged for program execution time.
+ */
+enum ChargeClass(val tag: String, val name: String, val description: String) derives Enumerated {
+
+  case NonCharged extends ChargeClass("nonCharged", "Non Charged", "Time that is not charged.")
+  case Partner    extends ChargeClass("partner",    "Partner",     "Time charged to a partner country / entity.")
+  case Program    extends ChargeClass("program",    "Program",     "Time charged to the science program.")
+
+}
