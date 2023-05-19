4c8864809138820a06f805ecca893f1263fb2d04
Shane Walker <pistes.clary_0w@icloud.com>
2023-05-19 12:59:44 -0400

    SetupTime

---
+++ modules/core/shared/src/main/scala/lucuma/core/model/sequence/SetupTime.scala
@@ -0,0 +1,34 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model.sequence
+
+import cats.Eq
+import lucuma.core.util.TimeSpan
+
+/**
+ * Setup time is an estimate for slewing, configuring, and acquiring the
+ * observation target.  There are estimates for a full setup from scratch
+ * and an alternative reacquisition time which doesn't require as much
+ * acquisition time.
+ */
+case class SetupTime(
+  full:          TimeSpan,
+  reacquisition: TimeSpan
+)
+
+object SetupTime {
+
+  val Zero: SetupTime =
+    SetupTime(
+      TimeSpan.Zero,
+      TimeSpan.Zero
+    )
+
+  given Eq[SetupTime] =
+    Eq.by { a => (
+      a.full,
+      a.reacquisition
+    )}
+
+}
