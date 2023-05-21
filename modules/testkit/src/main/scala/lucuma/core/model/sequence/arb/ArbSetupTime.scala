a04690c3e3256b5821740bd18cbdb38c84782ff3
Shane Walker <swalker2m@users.noreply.github.com>
2023-05-21 13:16:21 -0400

    SetupTime (#728)

---
+++ modules/testkit/src/main/scala/lucuma/core/model/sequence/arb/ArbSetupTime.scala
@@ -0,0 +1,35 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model.sequence
+package arb
+
+import lucuma.core.util.TimeSpan
+import lucuma.core.util.arb.ArbTimeSpan
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.*
+import org.scalacheck.Cogen
+import org.scalacheck.Gen
+
+
+trait ArbSetupTime {
+
+  import ArbTimeSpan.given
+
+  given Arbitrary[SetupTime] =
+    Arbitrary {
+      for {
+        f <- arbitrary[TimeSpan]
+        r <- arbitrary[TimeSpan]
+      } yield SetupTime(f, r)
+    }
+
+  given Cogen[SetupTime] =
+    Cogen[(TimeSpan, TimeSpan)].contramap { a => (
+      a.full,
+      a.reacquisition
+    )}
+
+}
+
+object ArbSetupTime extends ArbSetupTime
---
+++ modules/tests/shared/src/test/scala/lucuma/core/model/sequence/SetupTimeSuite.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model.sequence
+
+import cats.kernel.laws.discipline.*
+import cats.laws.discipline.arbitrary.*
+import lucuma.core.model.sequence.arb.ArbSetupTime
+import munit.*
+
+final class SetupTimeSuite extends DisciplineSuite {
+
+  import ArbSetupTime.given
+
+  checkAll("Eq[SetupTime]", EqTests[SetupTime].eqv)
+
+}
