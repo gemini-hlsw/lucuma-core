59eecf10b8d95aa0c203a781c75c543d7660907f
Shane Walker <pistes.clary_0w@icloud.com>
2023-06-08 13:44:27 -0400

    SmartGcalType tag update

--- modules/core/shared/src/main/scala/lucuma/core/enums/SmartGcalType.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/SmartGcalType.scala
@@ -1,20 +1,19 @@
 // Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package lucuma
-package core
-package enums
-import cats.syntax.eq._
+package lucuma.core.enums
+
 import lucuma.core.util.Enumerated
 
 /**
- * Enumerated type for "smart" calibration sequence tpes.
- * @group Enumerations (Generated)
+ * Enumerated type for "smart" calibration sequence types.
  */
-sealed abstract class SmartGcalType(
-  val tag: String
-) extends Product
-    with Serializable {
+enum SmartGcalType(val tag: String, val name: String) derives Enumerated {
+
+  case Arc           extends SmartGcalType("arc",            "Arc")
+  case Flat          extends SmartGcalType("flat",           "Flat")
+  case DayBaseline   extends SmartGcalType("day_baseline",   "Day Baseline")
+  case NightBaseline extends SmartGcalType("night_baseline", "Night Baseline")
 
   def fold[X](lamp: GcalLampType => X, baseline: GcalBaselineType => X): X =
     this match {
@@ -22,43 +21,6 @@ sealed abstract class SmartGcalType(
       case SmartGcalType.Flat          => lamp(GcalLampType.Flat)
       case SmartGcalType.DayBaseline   => baseline(GcalBaselineType.Day)
       case SmartGcalType.NightBaseline => baseline(GcalBaselineType.Night)
-    }
-
-}
-
-object SmartGcalType {
-
-  /** @group Constructors */
-  case object Arc extends SmartGcalType("Arc")
-
-  /** @group Constructors */
-  case object Flat extends SmartGcalType("Flat")
-
-  /** @group Constructors */
-  case object DayBaseline extends SmartGcalType("DayBaseline")
-
-  /** @group Constructors */
-  case object NightBaseline extends SmartGcalType("NightBaseline")
-
-  /** All members of SmartGcalType, in canonical order. */
-  val all: List[SmartGcalType] =
-    List(Arc, Flat, DayBaseline, NightBaseline)
-
-  /** Select the member of SmartGcalType with the given tag, if any. */
-  def fromTag(s: String): Option[SmartGcalType] =
-    all.find(_.tag === s)
-
-  /** Select the member of SmartGcalType with the given tag, throwing if absent. */
-  def unsafeFromTag(s: String): SmartGcalType =
-    fromTag(s).getOrElse(throw new NoSuchElementException(s"SmartGcalType: Invalid tag: '$s'"))
-
-  /** @group Typeclass Instances */
-  implicit val SmartGcalTypeEnumerated: Enumerated[SmartGcalType] =
-    new Enumerated[SmartGcalType] {
-      def all = SmartGcalType.all
-      def tag(a:                    SmartGcalType)         = a.tag
-      override def unsafeFromTag(s: String): SmartGcalType =
-        SmartGcalType.unsafeFromTag(s)
     }
 
 }
