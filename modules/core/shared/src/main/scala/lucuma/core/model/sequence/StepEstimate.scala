265c29eb248c1d1f626d4e2e8b6961bc49efb2fd
Shane Walker <pistes.clary_0w@icloud.com>
2023-08-02 15:08:43 -0400

    StepEstimate scala doc

--- modules/core/shared/src/main/scala/lucuma/core/model/sequence/StepEstimate.scala
+++ modules/core/shared/src/main/scala/lucuma/core/model/sequence/StepEstimate.scala
@@ -9,6 +9,15 @@ import cats.syntax.monoid.*
 import lucuma.core.data.Zipper
 import lucuma.core.util.TimeSpan
 
+/**
+ * Step planned time estimate, including configuration change costs and the exposure time and detector read/write
+ * overheads.  Configuration changes happen in parallel, so only the cost of the longest change contributes to the
+ * total cost for the step.  Similarly if the instrument has multiple detectors then they work in parallel so only
+ * one (the one that takes the most time) contributes to the total step cost.
+ *
+ * @param configChange estimated configuration change costs, where the Zipper focus is intended to be the maximum cost
+ * @param detector estimated exposure time and detector overheads, where the Zipper focus is intended to be the maximum cost
+ */
 case class StepEstimate(
   configChange: Option[Zipper[ConfigChangeEstimate]],
   detector:     Option[Zipper[DetectorEstimate]]
@@ -25,9 +34,20 @@ case class StepEstimate(
 
 object StepEstimate {
 
+  /**
+   * Zero time `StepEstimate`.
+   */
   val Zero: StepEstimate =
     StepEstimate(None, None)
 
+  /**
+   * Constructs from unordered lists of individual configuration change costs and detector estimates.  The longest
+   * estimate in each case contributes to the total `StepEstimate` and the remaining values are for informational
+   * purposes only.
+   *
+   * @param configChange all configuration change estimates
+   * @param detector all detector estimates
+   */
   def fromMax(
     configChange: List[ConfigChangeEstimate],
     detector:     List[DetectorEstimate]
