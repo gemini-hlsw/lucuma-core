5a6906e7cede9cd975c0fb50e0d19c52385e0bbe
Raúl Piaggio <rpiaggio@gemini.edu>
2023-11-07 10:05:46 -0300

    add tests

--- modules/tests/shared/src/test/scala/lucuma/core/model/sequence/AtomSuite.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/model/sequence/AtomSuite.scala
@@ -5,6 +5,7 @@ package lucuma.core.model.sequence
 
 import cats.kernel.laws.discipline.*
 import cats.laws.discipline.arbitrary.*
+import io.circe.testing.KeyCodecTests
 import lucuma.core.model.sequence.arb.*
 import lucuma.core.model.sequence.gmos.DynamicConfig.GmosNorth
 import lucuma.core.model.sequence.gmos.arb.*
@@ -23,7 +24,8 @@ final class AtomSuite extends DisciplineSuite {
 
   override val scalaCheckTestParameters = Test.Parameters.default.withMaxSize(10)
 
-  checkAll("Atom.Id", UidTests[Atom.Id].uid)
+  checkAll("Uid[Atom.Id]", UidTests[Atom.Id].uid)
+  checkAll("KeyCodec[Atom.Id]", KeyCodecTests[Atom.Id].keyCodec)
 
   checkAll("Eq[Atom[GmosNorth]]", EqTests[Atom[GmosNorth]].eqv)
   checkAll("Atom.id",             LensTests(Atom.id[GmosNorth]))
