744839b5cef680ca4b5b07489df4223c2b64f97c
Shane Walker <swalker2m@users.noreply.github.com>
2023-11-15 12:33:30 -0500

    Night syntax (#837)
    Co-authored-by: Shane Walker <pistes.clary_0w@icloud.com>

---
+++ modules/core/shared/src/main/scala/lucuma/core/model/syntax/NightOps.scala
@@ -0,0 +1,26 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model
+package syntax
+
+import lucuma.core.util.Timestamp
+import lucuma.core.util.TimestampInterval
+
+trait ToNightOps {
+
+  extension(self: Night) {
+
+    def toTimestampInterval: Option[TimestampInterval] = {
+      val bi = self.interval
+      for {
+        s <- Timestamp.fromInstantTruncated(bi.lower)
+        e <- Timestamp.fromInstantTruncated(bi.upper)
+      } yield TimestampInterval.between(s, e)
+    }
+
+  }
+
+}
+
+object night extends ToNightOps
