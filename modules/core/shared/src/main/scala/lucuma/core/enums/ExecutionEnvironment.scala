5d3c28f564b60102e41a7f94001d24f9a941934e
Raúl Piaggio <rpiaggio@gemini.edu>
2024-01-11 10:21:41 -0300

    add ExecutionEnvironment enum

---
+++ modules/core/shared/src/main/scala/lucuma/core/enums/ExecutionEnvironment.scala
@@ -0,0 +1,15 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.enums
+
+import cats.syntax.all.*
+import eu.timepit.refined.collection.NonEmpty
+import eu.timepit.refined.types.string.NonEmptyString
+import lucuma.core.util.Enumerated
+import lucuma.refined.*
+
+enum ExecutionEnvironment(val tag: String, val suffix: Option[NonEmptyString]) derives Enumerated:
+  case Development extends ExecutionEnvironment("DEVELOPMENT", "DEV".refined[NonEmpty].some)
+  case Staging     extends ExecutionEnvironment("STAGING", "STG".refined[NonEmpty].some)
+  case Production  extends ExecutionEnvironment("PRODUCTION", none)
