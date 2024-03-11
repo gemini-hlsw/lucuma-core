f3e3c0c659930171466c94e60bd7be5f641f39f2
Shane Walker <pistes.clary_0w@icloud.com>
2024-03-11 11:52:33 -0300

    Add ObservationReference

---
+++ modules/testkit/src/main/scala/lucuma/core/model/arb/ArbObservationReference.scala
@@ -0,0 +1,37 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model
+package arb
+
+import eu.timepit.refined.types.numeric.PosInt
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Cogen
+import org.scalacheck.Gen
+
+
+trait ArbObservationReference extends ArbReference {
+
+  import ArbProgramReference.given
+
+  given Arbitrary[ObservationReference] =
+    Arbitrary {
+      for {
+        p <- arbitrary[ProgramReference]
+        n <- arbitraryIndex
+      } yield ObservationReference(p, PosInt.unsafeFrom(n))
+    }
+
+  given Cogen[ObservationReference] =
+    Cogen[(ProgramReference, Int)].contramap { a => (
+      a.programReference,
+      a.observationIndex.value
+    )}
+
+  val observationReferenceStrings: Gen[String] =
+    referenceStrings[ObservationReference](_.label)
+
+}
+
+object ArbObservationReference extends ArbObservationReference
