b179f0b3f9068b03bd8e811fcb064801c916cbf2
Shane Walker <pistes.clary_0w@icloud.com>
2024-03-14 13:12:01 -0300

    step event updates

--- modules/core/shared/src/main/scala/lucuma/core/enums/StepStage.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/StepStage.scala
@@ -7,11 +7,15 @@ import lucuma.core.util.Enumerated
 
 enum StepStage(val tag: String, val name: String, val description: String) derives Enumerated {
 
+  case Abort          extends StepStage("abort",           "Abort Step",      "A step was aborted abruptly.")
+  case Continue       extends StepStage("continue",        "Continue Step",   "A paused step was continued.")
   case EndConfigure   extends StepStage("end_configure",   "End Configure",   "End of instrument configuration stage.")
   case EndObserve     extends StepStage("end_observe",     "End Observe",     "End of data collection for all datasets produced by this step.")
   case EndStep        extends StepStage("end_step",        "End Step",        "Step complete.")
+  case Pause          extends StepStage("pause",           "Pause Step",      "A step was paused mid-execution.")
   case StartConfigure extends StepStage("start_configure", "Start Configure", "Start of instrument configuration stage.")
   case StartObserve   extends StepStage("start_observe",   "Start Observe",   "Start of data collection for this step.")
   case StartStep      extends StepStage("start_step",      "Start Step",      "Step started.")
+  case Stop           extends StepStage("stop",            "Stop Step",       "A step was stopped gracefully before completion.")
 
 }
