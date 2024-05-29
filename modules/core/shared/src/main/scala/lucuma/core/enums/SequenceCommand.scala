8e0c43df46444a44a4d52e9e19a72e419b57fa49
Shane Walker <pistes.clary_0w@icloud.com>
2024-05-29 11:50:23 -0400

    Adds an isTerminal flag to SequenceCommand

--- modules/core/shared/src/main/scala/lucuma/core/enums/SequenceCommand.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/SequenceCommand.scala
@@ -5,12 +5,17 @@ package lucuma.core.enums
 
 import lucuma.core.util.Enumerated
 
-enum SequenceCommand(val tag: String, val name: String, val description: String) derives Enumerated {
+enum SequenceCommand(
+  val tag: String,
+  val name: String,
+  val isTerminal: Boolean,
+  val description: String
+) derives Enumerated {
 
-  case Abort    extends SequenceCommand("abort",    "Abort",    "Sequence was aborted without finishing any ongoing dataset(s).")
-  case Continue extends SequenceCommand("continue", "Continue", "Sequence was continued after having been paused.")
-  case Pause    extends SequenceCommand("pause",    "Pause",    "Sequence was temporarily paused.")
-  case Start    extends SequenceCommand("start",    "Start",    "Sequence execution started.")
-  case Stop     extends SequenceCommand("stop",     "Stop",     "Sequence execution was stopped, after first finishing any currently executing atom.")
+  case Abort    extends SequenceCommand("abort",    "Abort",    isTerminal = true,  "Sequence was aborted without finishing any ongoing dataset(s).")
+  case Continue extends SequenceCommand("continue", "Continue", isTerminal = false, "Sequence was continued after having been paused.")
+  case Pause    extends SequenceCommand("pause",    "Pause",    isTerminal = false, "Sequence was temporarily paused.")
+  case Start    extends SequenceCommand("start",    "Start",    isTerminal = false, "Sequence execution started.")
+  case Stop     extends SequenceCommand("stop",     "Stop",     isTerminal = true,  "Sequence execution was stopped, after first finishing any currently executing atom.")
 
 }
