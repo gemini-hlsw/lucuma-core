e701fcb3eb8fee233d07e23476b32fcb20a8943c
Shane Walker <swalker2m@users.noreply.github.com>
2024-07-25 20:22:19 -0400

    Add TimeAccountingCategory (#921)
    * Add TimeAccountingCategory
    
    * tlBaseVersion

--- modules/core/shared/src/main/scala/lucuma/core/enums/Partner.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/Partner.scala
@@ -9,18 +9,19 @@ enum Partner(
   val tag: String,
   val shortName: String,
   val longName: String,
-  val sites: Set[Site]
+  val sites: Set[Site],
+  val timeAccountingCategory: TimeAccountingCategory
 ) derives Enumerated {
 
   def abbreviation: String =
     tag.toUpperCase
 
-  case AR extends Partner("ar", "Argentina", "Argentina",            Site.all.toSet)
-  case BR extends Partner("br", "Brazil",    "Brazil",               Site.all.toSet)
-  case CA extends Partner("ca", "Canada",    "Canada",               Site.all.toSet)
-  case CL extends Partner("cl", "Chile",     "Chile",                Set(Site.GS))
-  case KR extends Partner("kr", "Korea",     "Republic of Korea",    Site.all.toSet)
-  case UH extends Partner("uh", "U of H",    "University of Hawaii", Set(Site.GN))
-  case US extends Partner("us", "USA",       "United States",        Site.all.toSet)
+  case AR extends Partner("ar", "Argentina", "Argentina",            Site.all.toSet, TimeAccountingCategory.AR)
+  case BR extends Partner("br", "Brazil",    "Brazil",               Site.all.toSet, TimeAccountingCategory.BR)
+  case CA extends Partner("ca", "Canada",    "Canada",               Site.all.toSet, TimeAccountingCategory.CA)
+  case CL extends Partner("cl", "Chile",     "Chile",                Set(Site.GS),   TimeAccountingCategory.CL)
+  case KR extends Partner("kr", "Korea",     "Republic of Korea",    Site.all.toSet, TimeAccountingCategory.KR)
+  case UH extends Partner("uh", "U of H",    "University of Hawaii", Set(Site.GN),   TimeAccountingCategory.UH)
+  case US extends Partner("us", "USA",       "United States",        Site.all.toSet, TimeAccountingCategory.US)
 
 }
