d9a364da57de49efbb530bfa207557aa09a42896
Hugo van Rijswijk <git@hugovr.nl>
2024-08-02 11:55:50 -0400

    Update JS Exports

--- modules/npm/src/main/typescript/main.d.ts
+++ modules/npm/src/main/typescript/main.d.ts
@@ -1,16 +1,3 @@
-export declare class PlotPoint {
-  constructor(instant: Date, airmass: number, altitude: number);
-  airmass: number;
-  altitude: number;
-  instant: Date;
-}
-
-export declare interface Site {}
-export declare const GN: Site;
-export declare const GS: Site;
-
-export declare function nightPlot(site: Site, start: Date, coords: string): PlotPoint[];
-
 export declare function deg2hms(deg: number): string;
 export declare function deg2dms(deg: number): string;
 export declare function hms2deg(hms: string): number;
