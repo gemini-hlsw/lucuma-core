72f52233cd305b20fdf352755237f847fd503794
Todd Burnside <tburnside@gemini.edu>
2024-10-23 12:37:19 -0700

    Only provide Eq for NewType if there is no Order instance

--- modules/core/shared/src/main/scala/lucuma/core/util/NewType.scala
+++ modules/core/shared/src/main/scala/lucuma/core/util/NewType.scala
@@ -13,13 +13,15 @@ import lucuma.core.optics.refinedPrism
 import monocle.Iso
 import monocle.Prism
 
+import scala.util.NotGiven
+
 /**
-  * Usage:
-  * ```
-  * object Name extends NewType[String]
-  * type Name = Name.Type
-  * ```
-  */
+ * Usage:
+ * ```
+ * object Name extends NewType[String]
+ * type Name = Name.Type
+ * ```
+ */
 trait NewType[Wrapped]:
   opaque type Type = Wrapped
 
@@ -28,20 +30,21 @@ trait NewType[Wrapped]:
   val value: Iso[Type, Wrapped] = Iso[Type, Wrapped](_.value)(apply)
 
   extension (t: Type)
-    inline def value: Wrapped = t
+    inline def value: Wrapped                           = t
     inline def modifyValue(f: Wrapped => Wrapped): Type = apply(f(value))
 
-  given (using CanEqual[Wrapped, Wrapped]): CanEqual[Type, Type] = CanEqual.derived
-  given (using eq: Eq[Wrapped]): Eq[Type]                        = eq
-  given (using enc: Encoder[Wrapped]): Encoder[Type]             = enc
-  given (using dec: Decoder[Wrapped]): Decoder[Type]             = dec
-  given (using disp: Display[Wrapped]): Display[Type]            = disp
-  given (using m: Monoid[Wrapped]): Monoid[Type]                 = m
-  given (using ord: Order[Wrapped]): Order[Type]                 = ord
-  given (using ord: Ordering[Wrapped]): Ordering[Type]           = ord
+  given (using CanEqual[Wrapped, Wrapped]): CanEqual[Type, Type]               = CanEqual.derived
+  // Only provide an Eq instance if it doesn't also provide Order
+  given (using eq:   Eq[Wrapped], noOrder: NotGiven[Order[Wrapped]]): Eq[Type] = eq
+  given (using enc:  Encoder[Wrapped]): Encoder[Type]                          = enc
+  given (using dec:  Decoder[Wrapped]): Decoder[Type]                          = dec
+  given (using disp: Display[Wrapped]): Display[Type]                          = disp
+  given (using m:    Monoid[Wrapped]): Monoid[Type]                            = m
+  given (using ord:  Order[Wrapped]): Order[Type]                              = ord
+  given (using ord:  Ordering[Wrapped]): Ordering[Type]                        = ord
 
 trait RefinedNewType[T, P](using RefinedType.AuxT[T Refined P, T]) extends NewType[T Refined P]:
-  private val TypeOps = new RefinedTypeOps[T Refined P, T]
-  def from(using Validate[T, P]): Prism[T, Type] = refinedPrism.andThen(value.reverse)
-  def from(t: T): Either[String, Type] = TypeOps.from(t).map(apply(_))
-  def unsafeFrom(x: T): Type = apply(TypeOps.unsafeFrom(x))
+  private val TypeOps                                  = new RefinedTypeOps[T Refined P, T]
+  def from(using Validate[T, P]): Prism[T, Type]       = refinedPrism.andThen(value.reverse)
+  def from(t:       T): Either[String, Type]           = TypeOps.from(t).map(apply(_))
+  def unsafeFrom(x: T): Type                           = apply(TypeOps.unsafeFrom(x))
