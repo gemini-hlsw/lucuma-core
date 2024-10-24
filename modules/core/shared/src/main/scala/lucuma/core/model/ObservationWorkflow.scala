ea77299e4a6e0dd884a138798cdbc80e5f7df8d8
Todd Burnside <tburnside@gemini.edu>
2024-10-24 13:21:32 -0700

    Add optics for ObservationWorkflow

--- modules/core/shared/src/main/scala/lucuma/core/model/ObservationWorkflow.scala
+++ modules/core/shared/src/main/scala/lucuma/core/model/ObservationWorkflow.scala
@@ -7,10 +7,21 @@ import cats.Eq
 import cats.derived.*
 import io.circe.Codec
 import lucuma.core.enums.ObservationWorkflowState
+import monocle.Focus
+import monocle.Lens
 
 final case class ObservationWorkflow(
   state: ObservationWorkflowState,
   validTransitions: List[ObservationWorkflowState],
   validationErrors: List[ObservationValidation]
 ) derives Codec, Eq
+
+
+object ObservationWorkflow:
+  val state: Lens[ObservationWorkflow, ObservationWorkflowState] = 
+    Focus[ObservationWorkflow](_.state)
+  val validTransitions: Lens[ObservationWorkflow, List[ObservationWorkflowState]] =
+    Focus[ObservationWorkflow](_.validTransitions)
+  val validationErrors: Lens[ObservationWorkflow, List[ObservationValidation]] =
+    Focus[ObservationWorkflow](_.validationErrors)
 
