3f05a80c2099b3642904bf09559cd6a99fe48423
Shane Walker <pistes.clary_0w@icloud.com>
2024-12-12 12:41:19 -0300

    Invitation update

--- modules/core/shared/src/main/scala/lucuma/core/model/UserInvitation.scala
+++ modules/core/shared/src/main/scala/lucuma/core/model/UserInvitation.scala
@@ -5,48 +5,26 @@ package lucuma.core.model
 
 import cats.Order
 import cats.syntax.all.*
-import eu.timepit.refined.cats.*
-import eu.timepit.refined.numeric.Positive
-import eu.timepit.refined.types.numeric.PosLong
 import io.circe.Decoder
-import io.circe.DecodingFailure
 import io.circe.Encoder
-import io.circe.Json
-import lucuma.core.util.RefinedNewType
+import lucuma.core.util.Gid
 import monocle.Prism
 
 import scala.util.matching.Regex
 
 /**
- * An invitation consists of an id (a positive Long) and a cleartext body (a 96-char lowercase hex
- * string). Invitations have a canonical string representation `id.body` where `id` is in lowercase
+ * An invitation consists of a ProgramUser.Id and a cleartext body (a 96-char
+ * lowercase hex string). Invitations have a canonical string representation
+ * `id.body` where `id` is the Long portion of the ProgramUser.Id in lowercase
  * hex, for example:
+ *
  * {{{
  * 10d.3b9e2adc5bffdac72f487a2760061bcfebc44037b69f2085c9a1ba10aa5d2d338421fc0d79f45cfd07666617ac4e2c89
  * }}}
  */
-case class UserInvitation(id: UserInvitation.Id, body: String)
+case class UserInvitation(id: ProgramUser.Id, body: String)
 
-object UserInvitation {
-
-  object Id extends RefinedNewType[Long, Positive] {
-
-    /** Id from hex string. */
-    val fromString: Prism[String, Id] =
-      Prism[String, Id] { sid =>
-        Either.catchOnly[NumberFormatException](java.lang.Long.parseLong(sid, 16)).flatMap { n =>
-          PosLong.from(n).map(Id.apply)
-        } .toOption
-      } { id =>
-        id.value.value.toHexString
-      }
-
-    given Encoder[Id] = id => Json.fromString(Id.fromString.reverseGet(id))
-    given Decoder[Id] = hc => hc.as[String].flatMap: s =>
-      Id.fromString.getOption(s).toRight(DecodingFailure("Invalid invitation id: $s", hc.history))
-
-  }
-  type Id = Id.Type
+object UserInvitation:
 
   extension(invitation: UserInvitation)
     def token: String = fromString.reverseGet(invitation)
@@ -56,10 +34,14 @@ object UserInvitation {
 
   val fromString: Prism[String, UserInvitation] =
     Prism[String, UserInvitation] {
-      case R(sid, body) => Id.fromString.getOption(sid).map(UserInvitation(_, body))
-      case _            => None
+      case R(puid, body) =>
+        val s = s"${Gid[ProgramUser.Id].tag.value}-${puid.dropWhile(_ === '0')}"
+        ProgramUser.Id.parse(s).map(UserInvitation(_, body))
+      case _             =>
+        None
      } { k =>
-      s"${k.id.value.value.toHexString}.${k.body}"
+      val id = f"${k.id.value.value.toHexString}%3s".replace(' ', '0')
+      s"$id.${k.body}"
     }
 
   given OrderUserInvitation: Order[UserInvitation] =
@@ -73,4 +55,3 @@ object UserInvitation {
 
   given Decoder[UserInvitation] =
     Decoder.decodeString.emap(s => fromString.getOption(s).toRight(s"Invalid user invitation: $s"))
-}
