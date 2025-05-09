74c217fb882984151d894693fae341a8896e7205
Shane Walker <swalker2m@users.noreply.github.com>
2025-05-09 14:52:57 -0400

    Require custom mask filename (#1104)

--- modules/testkit/src/main/scala/lucuma/core/model/sequence/f2/arb/ArbF2FpuMask.scala
+++ modules/testkit/src/main/scala/lucuma/core/model/sequence/f2/arb/ArbF2FpuMask.scala
@@ -23,18 +23,18 @@ trait ArbF2FpuMask:
         F2FpuMask.Imaging,
         Gen.oneOf(Enumerated[F2Fpu].all).map(F2FpuMask.Builtin(_)),
         for
-          f <- arbitrary[Option[NonEmptyString]]
+          f <- arbitrary[NonEmptyString]
           s <- arbitrary[F2CustomSlitWidth]
         yield F2FpuMask.Custom(f, s)
       )
     )
 
   given Cogen[F2FpuMask] =
-    Cogen[Option[Either[F2Fpu, (Option[String], F2CustomSlitWidth)]]]
+    Cogen[Option[Either[F2Fpu, (String, F2CustomSlitWidth)]]]
       .contramap {
         case F2FpuMask.Imaging => none
         case F2FpuMask.Builtin(b) => b.asLeft.some
-        case F2FpuMask.Custom(f, s) => (f.map(_.value), s).asRight.some
+        case F2FpuMask.Custom(f, s) => (f.value, s).asRight.some
       }
 
 object ArbF2FpuMask extends ArbF2FpuMask
