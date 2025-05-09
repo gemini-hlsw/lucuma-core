57e96ac387964947becf98b3707c94b31e88210e
Shane Walker <swalker2m@users.noreply.github.com>
2025-05-09 16:02:27 -0400

    Shortcut 4365: Remove custom slit width from static config (#1106)
    * Remove custom slit width from static config
    
    * switch to MosPreImaging enum

--- modules/core/shared/src/main/scala/lucuma/core/model/sequence/f2/F2StaticConfig.scala
+++ modules/core/shared/src/main/scala/lucuma/core/model/sequence/f2/F2StaticConfig.scala
@@ -5,29 +5,23 @@ package lucuma.core.model.sequence.f2
 
 import cats.Eq
 import cats.syntax.all.*
-import lucuma.core.enums.*
+import lucuma.core.enums.MosPreImaging
 import monocle.Focus
 import monocle.Lens
 
 case class F2StaticConfig(
-  mosPreImaging:          Boolean,
-  useElectronicOffseting: Boolean,
-  customSlitWidth:        Option[F2CustomSlitWidth]
+  mosPreImaging:          MosPreImaging,
+  useElectronicOffseting: Boolean
 )
 
 object F2StaticConfig:
   given Eq[F2StaticConfig] =
-    Eq.by(x => (x.mosPreImaging, x.useElectronicOffseting, x.customSlitWidth))
+    Eq.by(x => (x.mosPreImaging, x.useElectronicOffseting))
 
   /** @group Optics */
-  val mosPreImaging: Lens[F2StaticConfig, Boolean] =
+  val mosPreImaging: Lens[F2StaticConfig, MosPreImaging] =
     Focus[F2StaticConfig](_.mosPreImaging)
 
   /** @group Optics */
   val useElectronicOffsetting: Lens[F2StaticConfig, Boolean] =
     Focus[F2StaticConfig](_.useElectronicOffseting)
-
-  /** @group Optics */
-  val customSlitWidth: Lens[F2StaticConfig, Option[F2CustomSlitWidth]] =
-    Focus[F2StaticConfig](_.customSlitWidth)
-
