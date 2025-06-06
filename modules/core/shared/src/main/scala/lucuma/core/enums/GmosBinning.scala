07c0408379c88e9ef8910de8e81b2cf9fa3513f5
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-06-06 16:32:09 -0400

    So stupid

--- modules/core/shared/src/main/scala/lucuma/core/enums/GmosBinning.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/GmosBinning.scala
@@ -25,31 +25,31 @@ enum GmosBinning(
   case Four extends GmosBinning("Four", "4", "Four", 4.refined)
 
 object GmosXBinning extends NewType[GmosBinning]:
-  val One = GmosXBinning(GmosBinning.One)
-  val Two = GmosXBinning(GmosBinning.One)
-  val Four = GmosXBinning(GmosBinning.One)
+  val One  = GmosXBinning(GmosBinning.One)
+  val Two  = GmosXBinning(GmosBinning.Two)
+  val Four = GmosXBinning(GmosBinning.Four)
 
-  given Enumerated[GmosXBinning] = Enumerated.from(One, Two, Four).withTag(_.value.tag)
+  given Enumerated[GmosXBinning] = Enumerated.from(One, Two, Four).withTag(_.tag)
 
   extension(b: GmosXBinning)
-    inline def tag: String = b.value.tag
+    inline def tag: String       = b.value.tag
     inline def shortName: String = b.value.shortName
-    inline def longName: String = b.value.longName
-    inline def count: PosInt = b.value.count
+    inline def longName: String  = b.value.longName
+    inline def count: PosInt     = b.value.count
 
 type GmosXBinning = GmosXBinning.Type
 
 object GmosYBinning extends NewType[GmosBinning]:
-  val One = GmosYBinning(GmosBinning.One)
-  val Two = GmosYBinning(GmosBinning.One)
-  val Four = GmosYBinning(GmosBinning.One)
+  val One  = GmosYBinning(GmosBinning.One)
+  val Two  = GmosYBinning(GmosBinning.Two)
+  val Four = GmosYBinning(GmosBinning.Four)
 
-  given Enumerated[GmosYBinning] = Enumerated.from(One, Two, Four).withTag(_.value.tag)
+  given Enumerated[GmosYBinning] = Enumerated.from(One, Two, Four).withTag(_.tag)
 
   extension(b: GmosYBinning)
-    inline def tag: String = b.value.tag
+    inline def tag: String       = b.value.tag
     inline def shortName: String = b.value.shortName
-    inline def longName: String = b.value.longName
-    inline def count: PosInt = b.value.count
+    inline def longName: String  = b.value.longName
+    inline def count: PosInt     = b.value.count
 
 type GmosYBinning = GmosYBinning.Type
