de1aa9fc3ac6243f43c22a3efba134a5e626b192
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-06-07 10:59:33 -0400

    Add offset generator functions

---
+++ modules/core/shared/src/main/scala/lucuma/core/geom/OffsetGenerator.scala
@@ -0,0 +1,104 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.geom
+
+import cats.data.NonEmptyList
+import eu.timepit.refined.types.numeric.PosInt
+import lucuma.core.math.Angle
+import lucuma.core.math.Offset
+
+import scala.math.Pi
+import scala.math.ceil
+import scala.math.sqrt
+import scala.util.Random
+
+/**
+ * Based on the Python offset-generator script.
+ */
+object OffsetGenerator {
+
+  /**
+   * Generates a grid pattern of offsets.
+   */
+  def grid(
+    nx: PosInt,
+    ny: PosInt,
+    dp: Angle,
+    dq: Angle,
+    center: Offset = Offset.Zero
+  ): NonEmptyList[Offset] = {
+    val pStart = -(nx.value - 1) / 2.0
+    val qStart = (ny.value - 1) / 2.0
+
+    val offsets = for {
+      j <- 0 until ny.value
+      i <- 0 until nx.value
+    } yield {
+      val pOffset = dp * (pStart + i)
+      val qOffset = dq * (qStart - j)
+      center + Offset(pOffset.p, qOffset.q)
+    }
+    // can't be empty as nx and ny are 1 or more
+    NonEmptyList.fromListUnsafe(offsets.toList)
+  }
+
+  /**
+   * Generates a random pattern of offsets.
+   * Creates a grid and randomly places points near each grid element.
+   */
+  def random(
+    num: PosInt,
+    size: Angle,
+    center: Offset = Offset.Zero,
+    random: Random = new Random()
+  ): NonEmptyList[Offset] = {
+    val n = PosInt.unsafeFrom(ceil(sqrt(num.value.toDouble)).toInt)
+    val d = size * (1.0 / n.value.toDouble)
+
+    val gridPoints = grid(n, n, d, d, center)
+
+    val randomizedPoints = gridPoints.map: point =>
+      val pJitter = d * (random.nextDouble() - 0.5)
+      val qJitter = d * (random.nextDouble() - 0.5)
+      point + Offset(pJitter.p, qJitter.q)
+
+    // shuffle and take the requested number
+    val shuffled = random.shuffle(randomizedPoints.toList)
+    // can't be empty as num is 1 or more
+    NonEmptyList.fromListUnsafe(shuffled.take(num.value))
+  }
+
+  /**
+   * Generates a Fermat spiral pattern of offsets.
+   * Uses the golden angle for optimal packing.
+   */
+  def spiral(
+    num: PosInt,
+    size: Angle,
+    center: Offset = Offset.Zero,
+    random: Random = new Random()
+  ): NonEmptyList[Offset] = {
+    val θ = Angle.fromDoubleDegrees(137.50776)  // golden angle
+    val φ = Angle.fromDoubleRadians(random.nextDouble() * 2.0 * Pi)
+
+    // Calculate scale factor
+    val arcsecs = Angle.signedDecimalArcseconds.get(size).toDouble
+    val θʹ = θ * num.value.toDouble
+    val α = arcsecs / 2.0 / sqrt(num.value * (θʹ.cos * θʹ.cos + θʹ.sin * θʹ.sin))
+
+    val offsets = (0 until num.value).map { n =>
+      val sqrtN = sqrt(n.toDouble)
+      val angle = θ * n.toDouble + φ
+      val p = α * sqrtN * angle.cos
+      val q = α * sqrtN * angle.sin
+
+      center + Offset(
+        Angle.fromDoubleArcseconds(p).p,
+        Angle.fromDoubleArcseconds(q).q
+      )
+    }
+    // can't be empty as num is 1 or more
+    NonEmptyList.fromListUnsafe(offsets.toList)
+  }
+}
---
+++ modules/tests/shared/src/test/scala/lucuma/core/geom/OffsetGeneratorSuite.scala
@@ -0,0 +1,94 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.geom
+
+import eu.timepit.refined.types.numeric.PosInt
+import lucuma.core.math.Angle
+import lucuma.core.math.Offset
+import lucuma.core.math.syntax.int.*
+
+import scala.util.Random
+
+class OffsetGeneratorSuite extends munit.FunSuite:
+
+  test("grid generates correct number of offsets"):
+    val offsets = OffsetGenerator.grid(PosInt.unsafeFrom(3), PosInt.unsafeFrom(4), 1.arcsec, 2.arcsec)
+    assertEquals(offsets.length, 12) // 3 * 4
+
+  test("grid generates symmetric pattern around center"):
+    val offsets = OffsetGenerator.grid(PosInt.unsafeFrom(3), PosInt.unsafeFrom(3), 1.arcsec, 1.arcsec)
+    val center = Offset.Zero
+
+    assertEquals(offsets.length, 9)
+    assert(offsets.toList.contains(center))
+    val centerIndex = offsets.toList.indexOf(center)
+    assert(centerIndex >= 0)
+
+  test("grid handles single point"):
+    val offsets = OffsetGenerator.grid(PosInt.unsafeFrom(1), PosInt.unsafeFrom(1), 1.arcsec, 1.arcsec)
+    assertEquals(offsets.length, 1)
+    assertEquals(offsets.head, Offset.Zero)
+
+  // Test random generation
+  test("random generates correct number of offsets"):
+    val random = new Random(42) // Fixed seed
+    val offsets = OffsetGenerator.random(PosInt.unsafeFrom(10), 5.arcsec, Offset.Zero, random)
+    assertEquals(offsets.length, 10)
+
+  test("random generates offsets within expected bounds"):
+    val random = new Random(42)
+    val size = 10.arcsec
+    val offsets = OffsetGenerator.random(PosInt.unsafeFrom(20), size, Offset.Zero, random)
+
+    // All offsets should be within the diameter
+    val maxDistance = offsets.toList.map(_.distance(Offset.Zero)).maxBy(_.toSignedDoubleRadians)
+    assert(maxDistance.toSignedDoubleRadians <= size.toSignedDoubleRadians * 0.8)
+
+  // Test spiral generation
+  test("spiral generates correct number of offsets"):
+    val random = new Random(42)
+    val offsets = OffsetGenerator.spiral(PosInt.unsafeFrom(15), 8.arcsec, Offset.Zero, random)
+    assertEquals(offsets.length, 15)
+
+  test("spiral first point is at center"):
+    val random = new Random(42)
+    val offsets = OffsetGenerator.spiral(PosInt.unsafeFrom(10), 5.arcsec, Offset.Zero, random)
+
+    // First point should be close to center
+    val firstDistance = offsets.head.distance(Offset.Zero)
+    assert(firstDistance.toSignedDoubleRadians < 0.001)
+
+  test("spiral single point"):
+    val random = new Random(42)
+    val offsets = OffsetGenerator.spiral(PosInt.unsafeFrom(1), 5.arcsec, Offset.Zero, random)
+    assertEquals(offsets.length, 1)
+
+    // Single point should be at center
+    val distance = offsets.head.distance(Offset.Zero)
+    assert(distance.toSignedDoubleRadians < 0.001)
+
+  test("all generators handle zero size gracefully"):
+    val random = new Random(42)
+    val zeroSize = Angle.fromDoubleArcseconds(0.0)
+
+    // Grid with zero step size should work
+    val gridOffsets = OffsetGenerator.grid(PosInt.unsafeFrom(2), PosInt.unsafeFrom(2), zeroSize, zeroSize)
+    assertEquals(gridOffsets.length, 4)
+    assert(gridOffsets.toList.forall(_ == Offset.Zero))
+
+    // Random with zero size should keep points at center
+    val randomOffsets = OffsetGenerator.random(PosInt.unsafeFrom(3), zeroSize, Offset.Zero, random)
+    assertEquals(randomOffsets.length, 3)
+
+    // Spiral with zero size should keep points at center
+    val spiralOffsets = OffsetGenerator.spiral(PosInt.unsafeFrom(3), zeroSize, Offset.Zero, random)
+    assertEquals(spiralOffsets.length, 3)
+
+  test("negative step sizes work"):
+    val offsets = OffsetGenerator.grid(PosInt.unsafeFrom(2), PosInt.unsafeFrom(2), (-1).arcsec, (-1).arcsec)
+    assertEquals(offsets.length, 4)
+
+    // Should still generate valid offsets
+    assert(offsets.toList.forall(o => o.p.toAngle.toSignedDoubleRadians.isFinite))
+    assert(offsets.toList.forall(o => o.q.toAngle.toSignedDoubleRadians.isFinite))
