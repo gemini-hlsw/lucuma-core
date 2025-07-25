8a1c483bef1f554ebabdec05c0339fc228c3ccc0
Hugo van Rijswijk <git@hugovr.nl>
2025-07-25 16:23:14 +0200

    Export JS functions to parse IDs

--- modules/npm/src/main/typescript/main.d.ts
+++ modules/npm/src/main/typescript/main.d.ts
@@ -4,3 +4,12 @@ export declare function hms2deg(hms: string): number;
 export declare function dms2deg(dms: string): number;
 
 export declare function dateToLocalObservingNight(date: Date): string;
+
+export declare function parseAttachmentId(maybeAttachmentId: string): string | undefined;
+export declare function parseCallForProposalsId(maybeCallForProposalsId: string): string | undefined;
+export declare function parseGroupId(maybeGroupId: string): string | undefined;
+export declare function parseObservationId(maybeObservationId: string): string | undefined;
+export declare function parseProgramId(maybeProgramId: string): string | undefined;
+export declare function parseProgramNoteId(maybeProgramNoteId: string): string | undefined;
+export declare function parseProgramUserId(maybeProgramUserId: string): string | undefined;
+export declare function parseVisitId(maybeVisitId: string): string | undefined;
