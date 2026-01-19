93ce18bfc4c6bafb3f621618cda786afafac2ddd
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-01-19 14:56:29 -0300

    Add GOA data models for archive queries

---
+++ modules/catalog/src/main/scala/lucuma/catalog/goa/GoaInstrument.scala
@@ -0,0 +1,23 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.catalog.goa
+
+import lucuma.core.enums.Instrument
+
+object GoaInstrument:
+
+  def toGoaName(instrument: Instrument): Option[String] =
+    instrument match
+      case Instrument.GmosNorth  => Some("GMOS-N")
+      case Instrument.GmosSouth  => Some("GMOS-S")
+      case Instrument.Flamingos2 => Some("F2")
+      case Instrument.Gnirs      => Some("GNIRS")
+      case Instrument.Niri       => Some("NIRI")
+      case Instrument.Ghost      => Some("GHOST")
+      case Instrument.Gpi        => Some("GPI")
+      case Instrument.Gsaoi      => Some("GSAOI")
+      case Instrument.Alopeke    => Some("ALOPEKE")
+      case Instrument.Zorro      => Some("ZORRO")
+      case Instrument.Igrins2    => Some("IGRINS2")
+      case _                     => None
---
+++ modules/catalog/src/main/scala/lucuma/catalog/goa/GoaSummaryRecord.scala
@@ -0,0 +1,30 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.catalog.goa
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+final case class GoaSummaryRecord(
+  name:             String,
+  dataLabel:        Option[String],
+  ra:               Option[Double],
+  dec:              Option[Double],
+  instrument:       String,
+  observationType:  String,
+  observationClass: Option[String],
+  qaState:          Option[String],
+  utDateTime:       Option[LocalDateTime],
+  releaseDate:      Option[LocalDate],
+  programId:        Option[String],
+  observationId:    Option[String],
+  objectName:       Option[String],
+  exposure:         Option[Double],
+  disperser:        Option[String],
+  filter:           Option[String],
+  wavelength:       Option[Double],
+  airmass:          Option[Double],
+  azimuth:          Option[Double],
+  elevation:        Option[Double]
+)
