8b3e2bdef62e8a40b5e7200ce952121d02401edc
Shane Walker <swalker2m@users.noreply.github.com>
2026-05-06 08:47:53 -0400

    GHOST package object (#1384)

---
+++ modules/core/shared/src/main/scala/lucuma/core/model/sequence/ghost/package.scala
@@ -0,0 +1,11 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model.sequence.ghost
+
+import lucuma.core.math.Wavelength
+
+/**
+ * The fixed value for the GHOST central wavelength.
+ */
+val CentralWavelength: Wavelength = Wavelength.fromIntNanometers(655).get
