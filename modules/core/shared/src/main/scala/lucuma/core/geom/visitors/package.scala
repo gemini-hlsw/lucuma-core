9fd15586dffa63bcf908c02f6e6ab1cf40bad9b2
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-05-11 10:29:36 -0400

    Add science fov values for resident visitors

---
+++ modules/core/shared/src/main/scala/lucuma/core/geom/visitors/package.scala
@@ -0,0 +1,32 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.model.sequence.visitors
+
+import lucuma.core.math.Angle
+import lucuma.core.math.syntax.int.*
+
+/**
+ * The fixed value for the Alopeke science fov in speckle mode.
+ */
+val AlopekeSpeckleScienceFov: Angle = 3.arcsec
+
+/**
+ * The fixed value for the Alopeke science fov in wide field mode.
+ */
+val AlopekeWideFieldScienceFov: Angle = 35.arcsec
+
+/**
+ * The fixed value for the Zorro science fov in speckle mode.
+ */
+val ZorroSpeckleScienceFov: Angle = 3.arcsec
+
+/**
+ * The fixed value for the Zorro science fov in wide field mode.
+ */
+val ZorroWideFieldScienceFov: Angle = 35.arcsec
+
+/**
+ * The fixed value for the Maroon-X science fov. It is an octogonal IFU with 0.77" diameter.
+ */
+val MaroonXScienceFov: Angle = 770.milliarcseconds
