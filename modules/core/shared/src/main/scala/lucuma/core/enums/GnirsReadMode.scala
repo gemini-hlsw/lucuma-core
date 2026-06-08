071835f290bbde407355f42f0764e083554d3e61
Raúl Piaggio <rpiaggio@gemini.edu>
2026-06-08 14:09:05 -0300

    add digitalAverages to GnirsReadMoide

--- modules/core/shared/src/main/scala/lucuma/core/enums/GnirsReadMode.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/GnirsReadMode.scala
@@ -20,12 +20,13 @@ enum GnirsReadMode(
   val minimumExposureTime: TimeSpan,
   val readoutTimePerCoadd: TimeSpan,
   val readNoise: Int,
-  val lowNoiseReads: Int
+  val lowNoiseReads: Int,
+  val digitalAverages: Int // number of on-controller digital averages per coadd
 ) derives Enumerated, Display:
-    case VeryBright extends GnirsReadMode("VeryBright", "Very bright", "Very Bright Acquisition or High Background", 100.msTimeSpan, 190.msTimeSpan, 155, 1)
-    case Bright extends GnirsReadMode("Bright", "Bright", "Bright objects", 600.msTimeSpan, 690.msTimeSpan, 30, 1)
-    case Faint extends GnirsReadMode("Faint", "Faint", "Faint objects", 9000.msTimeSpan, 11400.msTimeSpan, 10, 16)
-    case VeryFaint extends GnirsReadMode("VeryFaint", "Very faint", "Very faint objects", 18000.msTimeSpan, 22310.msTimeSpan, 7, 32)
+    case VeryBright extends GnirsReadMode("VeryBright", "Very bright", "Very Bright Acquisition or High Background", 100.msTimeSpan, 190.msTimeSpan, 155, 1, 1)
+    case Bright extends GnirsReadMode("Bright", "Bright", "Bright objects", 600.msTimeSpan, 690.msTimeSpan, 30, 1, 16)
+    case Faint extends GnirsReadMode("Faint", "Faint", "Faint objects", 9000.msTimeSpan, 11400.msTimeSpan, 10, 16, 16)
+    case VeryFaint extends GnirsReadMode("VeryFaint", "Very faint", "Very faint objects", 18000.msTimeSpan, 22310.msTimeSpan, 7, 32, 16)
 
 object GnirsReadMode:
   // https://app.shortcut.com/lucuma/story/8557/set-gnirs-read-mode-based-on-exposure-time
