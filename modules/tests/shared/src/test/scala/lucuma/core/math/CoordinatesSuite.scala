22b1f9c95494662ed90d675e47df15fab14dcbfe
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-08-02 19:47:25 -0400

    Fix flaky coordinates test

--- modules/tests/shared/src/test/scala/lucuma/core/math/CoordinatesSuite.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/math/CoordinatesSuite.scala
@@ -127,16 +127,24 @@ final class CoordinatesSuite extends munit.DisciplineSuite {
   ) {
     val µas180 = Angle.Angle180.toMicroarcseconds
     val µas360 = µas180 * 2L
+    // Great-circle (slerp) interpolation is ill-conditioned for near-antipodal points:
+    // sin(δ) -> 0 in sin((1-f)δ)/sin(δ), so  error grows without as the separation approaches 180°
+    // Let's skip pairs within 1° of antipodal
+    val minGapµas = Angle.fromDoubleDegrees(1.0).toMicroarcseconds
 
     forAll { (c1: Coordinates, c2: Coordinates) =>
-      val sep = c1.angularDistance(c2)
-      val Δs  = (Range.BigDecimal(-1.0, 2.0, 0.1)).map { f =>
-        val stepSep  = c1.interpolate(c2, f.toDouble).angularDistance(c1).toMicroarcseconds
-        val fracSep  = (sep.toMicroarcseconds * f.abs).toLong
-        val fracSepʹ = if (fracSep <= µas180) fracSep else µas360 - fracSep
-        (stepSep - fracSepʹ).abs
+      val sep           = c1.angularDistance(c2)
+      val nearAntipodal = (µas180 - sep.toMicroarcseconds).abs < minGapµas
+      // Skip test cases very close to 180
+      !nearAntipodal ==> {
+        val Δs = (Range.BigDecimal(-1.0, 2.0, 0.1)).map { f =>
+          val stepSep  = c1.interpolate(c2, f.toDouble).angularDistance(c1).toMicroarcseconds
+          val fracSep  = (sep.toMicroarcseconds * f.abs).toLong
+          val fracSepʹ = if (fracSep <= µas180) fracSep else µas360 - fracSep
+          (stepSep - fracSepʹ).abs
+        }
+        Δs.max <= 55L
       }
-      assert(Δs.filter(_ > 55L).isEmpty)
     }
 
   }
