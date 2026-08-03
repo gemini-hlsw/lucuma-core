1995074ef95c9397c30784c1be8a90bc5fee4016
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-08-03 17:29:56 -0400

    Add enum for gmos mos acquisition type

---
+++ modules/core/shared/src/main/scala/lucuma/core/enums/GmosMosAcquisitionType.scala
@@ -0,0 +1,13 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.core.enums
+
+import lucuma.core.util.Enumerated
+
+/**
+ * Whether a GMOS MOS acquisition image is taken with the mask in or out.
+ */
+enum GmosMosAcquisitionType(val tag: String) derives Enumerated:
+  case MaskIn  extends GmosMosAcquisitionType("MaskIn")
+  case MaskOut extends GmosMosAcquisitionType("MaskOut")
