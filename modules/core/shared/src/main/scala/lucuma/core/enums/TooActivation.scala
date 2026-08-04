4da6461d1a3bac15b23c3bf808758c42938d27b8
Shane Walker <pistes.clary_0w@icloud.com>
2026-08-04 15:18:33 -0400

    add default execution environment

--- modules/core/shared/src/main/scala/lucuma/core/enums/TooActivation.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/TooActivation.scala
@@ -23,3 +23,15 @@ enum TooActivation(val tag: String, val label: String) derives Enumerated:
 
   /** Observed as soon as possible, displacing ongoing work where permitted. */
   case Interrupting extends TooActivation("interrupting", "Interrupting")
+
+  /**
+   * The execution requirement an observation with this activation gets when it
+   * states none of its own -- and, because the default acts as a floor, the
+   * least restrictive requirement it can end up with.  A rapid or interrupting
+   * Target of Opportunity is triggered precisely because the target will not
+   * wait, so allowing it to be interrupted would defeat the purpose.
+   */
+  def executionRequirementDefault: ExecutionRequirement =
+    this match
+      case None  | Standard     => ExecutionRequirement.Unconstrained
+      case Rapid | Interrupting => ExecutionRequirement.Uninterruptible
