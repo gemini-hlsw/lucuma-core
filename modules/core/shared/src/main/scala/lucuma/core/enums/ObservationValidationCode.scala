da98c37e875652d816d7264850525be29cd42157
Rob Norris <rob_norris@mac.com>
2026-08-06 11:39:27 -0500

    severity

--- modules/core/shared/src/main/scala/lucuma/core/enums/ObservationValidationCode.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/ObservationValidationCode.scala
@@ -5,40 +5,52 @@ package lucuma.core.enums
 
 import lucuma.core.util.Enumerated
 
+import ObservationValidationCode.Severity
+import ObservationValidationCode.Severity.Fatal
+
 enum ObservationValidationCode(
   val tag: String,
   val name: String,
-  val description: String
+  val description: String,
+  val severity: Severity
 ) derives Enumerated:
-  case ConfigurationError extends ObservationValidationCode("configuration_error", "Configuration Error", "The observation is not configured correctly and cannot be executed")
-  case CallForProposalsError extends ObservationValidationCode("cfp_error", "Call for Proposals Error", "Not valid for the selected Call for Proposals")
-  case ItcError extends ObservationValidationCode("itc_error", "ITC Error", "Integration time is unavailable.")
+  case ConfigurationError extends ObservationValidationCode("configuration_error", "Configuration Error", "The observation is not configured correctly and cannot be executed", Fatal)
+  case CallForProposalsError extends ObservationValidationCode("cfp_error", "Call for Proposals Error", "Not valid for the selected Call for Proposals", Fatal)
+  case ItcError extends ObservationValidationCode("itc_error", "ITC Error", "Integration time is unavailable.", Fatal)
   case ConfigurationRequestUnavailable 
     extends ObservationValidationCode(
       "config_request_unavailable", 
       "Unknown Approval Status", 
-      ObservationValidationCode.ConfigurationRequestMsg.Unavailable
+      ObservationValidationCode.ConfigurationRequestMsg.Unavailable, 
+      Fatal
     )
   case ConfigurationRequestNotRequested
     extends ObservationValidationCode(
       "config_request_not_requested", 
       "Needs Approval", 
-      ObservationValidationCode.ConfigurationRequestMsg.NotRequested
+      ObservationValidationCode.ConfigurationRequestMsg.NotRequested, 
+      Fatal
     )
   case ConfigurationRequestDenied
     extends ObservationValidationCode(
       "config_request_denied", 
       "Denied", 
-      ObservationValidationCode.ConfigurationRequestMsg.Denied
+      ObservationValidationCode.ConfigurationRequestMsg.Denied, 
+      Fatal
     )
   case ConfigurationRequestPending
     extends ObservationValidationCode(
       "config_request_pending", 
       "Approval Pending", 
-      ObservationValidationCode.ConfigurationRequestMsg.Pending
+      ObservationValidationCode.ConfigurationRequestMsg.Pending, 
+      Fatal
     )
 
 object ObservationValidationCode:
+
+  enum Severity:
+    case Fatal, Nonfatal
+
   object ConfigurationRequestMsg:
     val Unavailable  = "Configuration approval status could not be determined."
     val NotRequested = "Configuration is unapproved (approval has not been requested)."
