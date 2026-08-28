13baa831cd933b4b5c9d05c748a8c162705bb90b
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-08-28 11:01:03 -0400

    Add display instance

--- modules/core/shared/src/main/scala/lucuma/core/enums/ProgramStatus.scala
+++ modules/core/shared/src/main/scala/lucuma/core/enums/ProgramStatus.scala
@@ -3,9 +3,10 @@
 
 package lucuma.core.enums
 
+import lucuma.core.util.Display
 import lucuma.core.util.Enumerated
 
-enum ProgramStatus(val tag: String, val name: String) derives Enumerated:
+enum ProgramStatus(val tag: String, val name: String) derives Enumerated, Display:
   case Active     extends ProgramStatus("active",     "Active")
   case Inactive   extends ProgramStatus("inactive",   "Inactive")
   case Complete   extends ProgramStatus("complete",   "Complete")
