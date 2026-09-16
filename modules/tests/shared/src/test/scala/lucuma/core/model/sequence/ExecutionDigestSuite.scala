bf81bf5beeafc9441fce8f20b95805558a58446f
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-09-16 14:06:44 -0300

    Remove lens tests

--- modules/tests/shared/src/test/scala/lucuma/core/model/sequence/ExecutionDigestSuite.scala
+++ modules/tests/shared/src/test/scala/lucuma/core/model/sequence/ExecutionDigestSuite.scala
@@ -4,19 +4,9 @@
 package lucuma.core.model.sequence
 
 import cats.kernel.laws.discipline.*
-import eu.timepit.refined.cats.*
-import eu.timepit.refined.scalacheck.all.*
 import lucuma.core.model.sequence.arb.ArbExecutionDigest.given
-import lucuma.core.model.sequence.arb.ArbSequenceDigest.given
-import lucuma.core.model.sequence.arb.ArbSetupTime.given
-import monocle.law.discipline.*
 import munit.*
 
 final class ExecutionDigestSuite extends DisciplineSuite:
 
-  checkAll("Eq[ExecutionDigest]",              EqTests[ExecutionDigest].eqv)
-  checkAll("ExecutionDigest.setup",            LensTests(ExecutionDigest.setup))
-  checkAll("ExecutionDigest.setupCount",       LensTests(ExecutionDigest.setupCount))
-  checkAll("ExecutionDigest.calibrationCount", LensTests(ExecutionDigest.calibrationCount))
-  checkAll("ExecutionDigest.acquisition",      LensTests(ExecutionDigest.acquisition))
-  checkAll("ExecutionDigest.science",          LensTests(ExecutionDigest.science))
+  checkAll("Eq[ExecutionDigest]", EqTests[ExecutionDigest].eqv)
